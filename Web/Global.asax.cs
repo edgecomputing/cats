a6f76b91f56d68aa43904a9103f6a529d83f32e5
Banty <bantyder@gmail.com>
2013-06-24 15:55:22 +0300

    Configure Dependency Injection

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Cats.Infrastructure;
 
 namespace Cats
 {
@@ -17,6 +18,8 @@ namespace Cats
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ Web/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cats.Data.UnitWork;
+using Ninject;
+
+namespace Cats.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            this.kernel = new StandardKernel();
+            AddBindings();
+        }
+
+       
+
+    public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        
+        private void AddBindings()
+        {
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
