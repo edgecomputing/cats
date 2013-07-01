f49f225ba980734c408dd92a55aff24b3c984b48
Beka Mengistu <bekalive@gmail.com>
2013-07-01 05:50:36 -0700

    Created-Migrate script for Procurement schema and transporter table Created-Procurement Area.

---
+++ Migrations/Cats.Migration/201307011512_procurment_transporter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Cats.Migration
+{
+    [Migration(201307011512)]
+    public class ProcurementSchemaMigration : FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("Procurement");
+            Create.Table("Transporter").InSchema("Procurement")
+                  .WithColumn("TransporterID").AsInt32().NotNullable().PrimaryKey().Identity()
+                  .WithColumn("Region").AsString().Nullable()
+                  .WithColumn("SubCity").AsString().Nullable()
+                  .WithColumn("Zone").AsString().Nullable()
+                  .WithColumn("Woreda").AsString().Nullable()
+                  .WithColumn("Kebele").AsString().Nullable()
+                  .WithColumn("HouseNo").AsString().Nullable()
+                  .WithColumn("TelephoneNo").AsString().Nullable()
+                  .WithColumn("MobileNo").AsString().Nullable()
+                  .WithColumn("Email").AsString().Nullable()
+
+                  .WithColumn("Ownership").AsString().Nullable()
+                  .WithColumn("VehicleCount").AsInt32().Nullable()
+                  .WithColumn("LiftCapacityFrom").AsDecimal().Nullable()
+                  .WithColumn("LiftCapacityTo").AsDecimal().Nullable()
+                  .WithColumn("LiftCapacityTotal").AsDecimal().Nullable()
+                  .WithColumn("Capital").AsDecimal().Nullable()
+                  .WithColumn("EmployeeCountMale").AsInt32().Nullable()
+                  .WithColumn("EmployeeCountFemale").AsInt32().Nullable()
+
+                .WithColumn("OwnerName").AsString().Nullable()
+                .WithColumn("OwnerMobile").AsString().Nullable()
+                .WithColumn("ManagerName").AsString().Nullable()
+                .WithColumn("ManagerMobile").AsString().Nullable()
+
+                .WithColumn("ExperienceFrom").AsDateTime().Nullable()
+                .WithColumn("ExperienceTo").AsDateTime().Nullable();
+                 
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Transporter").InSchema("Procurement");
+            Delete.Schema("Procurement");
+        }
+    }
+}
+
---
+++ Web/Areas/Procurement/ProcurementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Cats.Areas.Procurement
+{
+    public class ProcurementAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Procurement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Procurement_default",
+                "Procurement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
