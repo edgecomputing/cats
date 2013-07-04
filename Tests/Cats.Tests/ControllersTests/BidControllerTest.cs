d696e9466fb58aec72dfa1fd31d6ea478d5c6861
Fisseha Abebe <abebefisseha5@gmail.com>
2013-07-04 14:40:56 +0300

    Add Bid Controller Test

--- Tests/Cats.Tests/ControllersTests/BidControllerTest.cs
+++ Tests/Cats.Tests/ControllersTests/BidControllerTest.cs
@@ -18,19 +18,48 @@ namespace Cats.Tests.ControllersTests
         private IBidDetailService MockBidDetail;
         private IAdminUnitService MockAdminUnitService;
         private BidController _bidController;
+        private List<Bid> _bids;
         
         public BidControllerTest()
           {
 
-            //List<Bid> bidTest = new List<Bid>();
-            //{
-            //    new Bid()
-            //}
-            //;
-            // List<AdminUnit> adminUnitTest=new List<AdminUnit>();
-            //{
-            //    new AdminUnit() {AdminUnitID = 1, Name = "Afar", NameAM = null, AdminUnitTypeID = 2, ParentID = 1};
-            //}
+            List<Bid> bidTest = new List<Bid>();
+              {
+                  new Bid() { BidID=1,BidNumber ="PP452",StartDate=new DateTime(2012/10/10),EndDate=new DateTime(2013/12/11)};
+                  new Bid() { BidID = 2,BidNumber ="AAA123",StartDate = new DateTime(2012 / 10 / 10), EndDate = new DateTime(2013 / 12 / 11) };
+                  new Bid() { BidID = 3,BidNumber="QW123",StartDate = new DateTime(2012 / 10 / 10), EndDate = new DateTime(2013 / 12 / 11) };
+              }
+            ;
+             List<AdminUnit> adminUnitTest=new List<AdminUnit>();
+            {
+                new AdminUnit() {AdminUnitID = 1, Name = "Afar", NameAM = null, AdminUnitTypeID = 2, ParentID = 1};
+            }
+            ;
+
+            List<BidDetail> bidDetailTest=new List<BidDetail>();
+              {
+                  new BidDetail()
+                      {
+                          BidDetailID = 1,
+                          BidID = 1,
+                          RegionID = 2,
+                          AmountForPSNPProgram = 200,
+                          AmountForReliefProgram = 300,
+                          BidDocumentPrice = 10,
+                          CBO = 1000
+                      };
+                  new BidDetail()
+                  {
+                      BidDetailID = 2,
+                      BidID = 1,
+                      RegionID = 2,
+                      AmountForPSNPProgram = 200,
+                      AmountForReliefProgram = 300,
+                      BidDocumentPrice = 10,
+                      CBO = 1000
+                  };
+              }
+
 
              //Mock the Regional Request Service Using Moq 
             Mock<IBidService> mockBidService = new Mock<IBidService>();
@@ -38,13 +67,13 @@ namespace Cats.Tests.ControllersTests
             Mock<IAdminUnitService> mockAdminUnitService=new Mock<IAdminUnitService>();
 
 
-            //mockBidService.Setup(m => m.GetAllBid()).Returns(bidTest);
+            mockBidService.Setup(m => m.GetAllBid()).Returns(bidTest);
            // mockAdminUnitService.Setup(m => m.FindBy(au => au.AdminUnitTypeID==2)).Returns(adminUnitTest);
            
 
             this.MockAdminUnitService = mockAdminUnitService.Object;
             this.MockBidService = mockBidService.Object;
-            //this.MockBidDetail = MockBidDetail.Object;
+            
 
 
           }
@@ -53,25 +82,61 @@ namespace Cats.Tests.ControllersTests
         {
             _bidController=new BidController(MockBidService,MockBidDetail,MockAdminUnitService);
         }
+
+        [Test]
+        public void Bid_Controller_Constructor_Test()
+        {
+            try
+            {
+                _bidController=new BidController(MockBidService, MockBidDetail, MockAdminUnitService);
+            }
+            catch (Exception e)
+            {
+
+                Assert.Fail(e.Message);
+            }
+        }
         
+        [Test]
+        public void Can_fetch_all_Bid_Lists()
+        {
+             List<Bid> expected = new List<Bid>();
+              {
+                  new Bid() { BidID=1,BidNumber ="PP452",StartDate=new DateTime(2012/10/10),EndDate=new DateTime(2013/12/11)};
+                  new Bid() { BidID = 2,BidNumber ="AAA123",StartDate = new DateTime(2012 / 10 / 10), EndDate = new DateTime(2013 / 12 / 11) };
+              }
+            ;
+            List<Bid> actual = MockBidService.GetAllBid();
+            Assert.AreEqual(actual.Count,expected.Count);
+        }
+
         [Test]
         public void Bid_Index_Test()
         {
             ActionResult actual = _bidController.Index();
             ViewResult result = actual as ViewResult;
-            //String viewName = "Index";
             Assert.IsNotNull(result);
-            Assert.AreEqual("", result.ViewName);
         }
+
+
         [Test]
-        public void Create_Bid_Test()
+        public void Edit_Bid_Test()
         {
-            List<Bid> bidTests=new List<Bid>();
+            List<Bid> expected = new List<Bid>();
             {
-                new Bid {BidID=1,BidNumber="ADC",StartDate="",EndDate=""};
+                new Bid() { BidID = 1, BidNumber = "PP452", StartDate = new DateTime(2012 / 10 / 10), EndDate = new DateTime(2013 / 12 / 11) };
+                new Bid() { BidID = 2, BidNumber = "AAA123", StartDate = new DateTime(2012 / 10 / 10), EndDate = new DateTime(2013 / 12 / 11) };
             }
-            var result = _bidController.Create(bidTests);
-            Assert.IsTrue(result);
+            ;
+            List<Bid> actual = (List<Bid>) MockBidService.Get(m => m.BidID == 1);
+            Assert.AreEqual(actual,expected);
+
+            foreach (var bidExpected in expected)
+            {
+                Assert.IsTrue(actual.Contains(actual.Find(r => r.BidID == bidExpected.BidID)));
+            }
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
