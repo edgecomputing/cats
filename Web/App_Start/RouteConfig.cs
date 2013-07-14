6089528784a4ebd34d487c6eb11df3d0b501e5c7
Yared Ayalew <yareda@gmail.com>
2013-07-14 21:57:33 +0300

    Add Logout named route

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -19,6 +19,11 @@ namespace Cats
                 defaults:new {controller="Account",action="Login"}
                 );
             routes.MapRoute(
+                name:"Logout",
+                url:"logout",
+                defaults:new {controller="Account",action="Logout"}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
