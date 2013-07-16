505e2dde34d25744110664d90f244501be77f38b
Beka Mengistu <bekalive@gmail.com>
2013-07-15 21:38:46 -0700

    Winner and Price Quotation Registration

--- Web/Areas/Procurement/Models/PriceQuotationModel.cs
+++ Web/Areas/Procurement/Models/PriceQuotationModel.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using Cats.Models;
 namespace Cats.Areas.Procurement.Models
 {
     public class PriceQuotationModel
     {
         public int BidPlanID { get; set; }
         public int TransporterID { get; set; }
+        public int RegionID { get; set; }
+    }
+    public class PriceQuotationModelDetailModel
+    {
+       // public string SourceWarehouse { get; set; }
+        public Hub SourceWarehouse { get; set; }
+        public string DestinationZone { get; set; }
+        public AdminUnit DestinationWoreda { get; set; }
+        public string DestinationWoredaName { get; set; }
+        
+        public string RegionName { get; set; }
         public int RegionID { get; set; }
     }
 }
