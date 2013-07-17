88d2061e7bd3bdc83347d725c70c8116bd14fc0f
Beka Mengistu <bekalive@gmail.com>
2013-07-17 05:08:06 -0700

    Bid Quotation registration

--- Web/Areas/Procurement/Controllers/PriceQuotationController.cs
+++ Web/Areas/Procurement/Controllers/PriceQuotationController.cs
@@ -24,6 +24,7 @@ namespace Cats.Areas.Procurement.Controllers
         private readonly ITransportBidQuotationService _bidQuotationService;
         private readonly IBidService _bidService;
         private readonly ITransporterService _transporterService;
+   
 
         public PriceQuotationController(ITransportBidPlanService transportBidPlanServiceParam
                                             , IAdminUnitService adminUnitServiceParam
@@ -48,7 +49,7 @@ namespace Cats.Areas.Procurement.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            ViewBag.BidPlanID = new SelectList(_bidService.GetAllBid(), "TransportBidPlanID", "BidNumber");
+            ViewBag.BidPlanID = new SelectList(_bidService.GetAllBid(), "BidID", "BidNumber");
             ViewBag.RegionID = new SelectList(_adminUnitService.FindBy(t => t.AdminUnitTypeID == 2), "AdminUnitID", "Name");
             ViewBag.TransporterID = new SelectList(_transporterService.GetAllTransporter(), "TransporterID", "Name");
             PriceQuotationModel model = new PriceQuotationModel();
@@ -56,7 +57,49 @@ namespace Cats.Areas.Procurement.Controllers
             return View(model);
             //return View();
         }
+        [HttpPost]
+        public ActionResult Edit(TransportBidQuotation transportQuote)
+        {
+           // if (ModelState.IsValid)
+            {
+                List<TransportBidQuotation> existing = 
+                    _bidQuotationService.FindBy(t => t.BidID == transportQuote.BidID  
+                                               && t.TransporterID == transportQuote.TransporterID
+                                               && t.SourceID == transportQuote.SourceID
+                                               && t.DestinationID == transportQuote.DestinationID
+                                               );
+                if (existing.Count == 1)
+                {
+                    TransportBidQuotation edited = existing[0];
+//                    transportQuote.TransportBidQuotationID = edited.TransportBidQuotationID;
+                    edited.Tariff = transportQuote.Tariff;
+                    edited.Remark = transportQuote.Remark;
+                    edited.Position = transportQuote.Position;
+                    edited.IsWinner = transportQuote.IsWinner;
+                    _bidQuotationService.UpdateTransportBidQuotation(edited);
+                }
+                else
+                {
+                    _bidQuotationService.AddTransportBidQuotation(transportQuote);
+                }
+                return View(transportQuote);
+               // _transportBidPlanService.UpdateTransportBidPlan(transportbidplan);
+               // return RedirectToAction("EditStart",PriceQuotationModel { BidPlanID = transportQuote.BidID, TransporterID = transportQuote.TransporterID, RegionID = 2 });
+              //  return EditStart(new PriceQuotationModel { BidPlanID = transportQuote.BidID, TransporterID = transportQuote.TransporterID, RegionID = 2 });
+            }
+           // return RedirectToAction("Index");
+        }
+        public Dictionary<string, TransportBidQuotation> organizeList(List<TransportBidQuotation> quoteList)
+        {
+            System.Collections.Generic.Dictionary<string, TransportBidQuotation> ret = new Dictionary<string, TransportBidQuotation>();
 
+            foreach (var i in quoteList)
+            {
+                string hash = i.BidID + "_" + i.TransporterID + "_" + i.SourceID + "_" + i.DestinationID;
+                ret.Add(hash, i);// = i;
+            }
+            return ret;
+        }
         //
         // GET: /Procurement/RFQ/EditStart
         [HttpPost]
@@ -64,17 +107,31 @@ namespace Cats.Areas.Procurement.Controllers
         {
 
             ViewBag.ModelFilter = model;
-
+            int bidID = model.BidPlanID;
+            
             ViewBag.BidPlanID = new SelectList(_bidService.GetAllBid(), "TransportBidPlanID", "BidNumber");
             ViewBag.RegionID = new SelectList(_adminUnitService.FindBy(t => t.AdminUnitTypeID == 2), "AdminUnitID", "Name");
             ViewBag.TransporterID = new SelectList(_transporterService.GetAllTransporter(), "TransporterID", "Name");
            // PriceQuotationModel model = new PriceQuotationModel();
             // return RedirectToAction("Edit");
             ViewBag.SelectedTransporter = _transporterService.FindById(model.TransporterID);
+            Bid SelectedBid = _bidService.FindById(bidID);
+            ViewBag.SelectedBid =SelectedBid;
+            int bidPlanID = SelectedBid.TransportBidPlanID;
+
+            List<TransportBidQuotation> transporterQuote = _bidQuotationService.FindBy(t => t.BidID == bidID && t.TransporterID == model.TransporterID);
+            Dictionary<string, TransportBidQuotation> transporterQuoteDictionary = organizeList(transporterQuote);
+
+            ViewBag.transporterQuoteHash = transporterQuoteDictionary;
+            
+            foreach (var i in transporterQuoteDictionary.Values)
+            {
+            }
+
             {
                 //ViewBag.RegionID = new SelectList(_adminUnitService.GetAllAdminUnit(), "AdminUnitID", "Name", RegionID);
                 ViewBag.SelectedRegion = _adminUnitService.FindById(model.RegionID);
-                TransportBidPlan bidPlan = _transportBidPlanService.FindById(model.BidPlanID);
+                TransportBidPlan bidPlan = _transportBidPlanService.FindById(bidPlanID);
 
                 List<TransportBidPlanDetail> regionalPlan = _transportBidPlanDetailService.FindBy(t => t.BidPlanID == model.BidPlanID && t.Destination.AdminUnit2.AdminUnit2.AdminUnitID == model.RegionID);
 
@@ -88,15 +145,19 @@ namespace Cats.Areas.Procurement.Controllers
 
                 var regionPlanDistinct = (from rg in regionalPlanSorted
 
-                                          select new RfqViewModel
+                                          select new PriceQuotationModelDetailModel
                                           {
-                                              SourceWarehouse = rg.Source.Name,
+                                             
+                                              SourceWarehouse=rg.Source,
+
                                               DestinationZone = rg.Destination.AdminUnit2.Name,
                                               RegionName = rg.Destination.AdminUnit2.AdminUnit2.Name,
-                                              DestinationWoreda = rg.Destination.Name
+                                              DestinationWoreda = rg.Destination,
+                                              DestinationWoredaName = rg.Destination.Name
+
                                           })
 
-                 .GroupBy(rg => new { rg.SourceWarehouse, rg.DestinationZone, rg.DestinationWoreda })
+                 .GroupBy(rg => new { rg.SourceWarehouse.Name, rg.DestinationZone, rg.DestinationWoredaName })
 
                  .Select(s => s.FirstOrDefault());
 
@@ -115,7 +176,7 @@ namespace Cats.Areas.Procurement.Controllers
             return View(model);
             //return View();
         }
-        [HttpPost]
+        /*[HttpPost]
         public ActionResult Edit(PriceQuotationModel model)
         {
             ViewBag.BidPlanID = new SelectList(_bidService.GetAllBid(), "TransportBidPlanID", "BidNumber");
@@ -125,6 +186,6 @@ namespace Cats.Areas.Procurement.Controllers
             // return RedirectToAction("Edit");
             return View(model);
             //return View();
-        }
+        }*/
     }
 }
