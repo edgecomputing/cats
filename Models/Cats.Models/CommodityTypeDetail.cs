a8d89a579a0d27905a6737b6c676dccf0417d660
Fisseha Abebe <abebefisseha5@gmail.com>
2013-07-18 09:44:35 +0300

    CATS 78 Add models for HRD

---
+++ Models/Cats.Models/CommodityTypeDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cats.Models
+{
+    public class CommodityTypeDetail
+    {
+        public int CommodityTypeDetailID { get; set; }
+        public int HumanitarianRequirementDetailID { get; set; }
+        public int CommodityTypeID { get; set; }
+        public long Amount { get; set; }
+
+        public virtual HumanitarianRequirementDetail HumanitarianRequirementDetail { get; set; }
+        public virtual CommodityType CommodityType { get; set; }
+
+    }
+}
---
+++ Models/Cats.Models/RationDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cats.Models
+{
+    public class RationDetail
+    {
+        public int RationDetailID { get; set; }
+        public int RationID { get; set; }
+        public int CommodityTypeID { get; set; }
+        public decimal Rate { get; set; }
+
+        public virtual Ration Ration { get; set; }
+    }
+}
