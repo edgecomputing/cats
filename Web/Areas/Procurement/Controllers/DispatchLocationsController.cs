cc2340256c97d092cbf98825918eaa1d74dad320
Fisseha Abebe <abebefisseha5@gmail.com>
2013-07-19 16:01:37 +0300

    Replace html Grids by Kendo Grid

--- Web/Areas/Procurement/Controllers/DispatchLocationsController.cs
+++ Web/Areas/Procurement/Controllers/DispatchLocationsController.cs
@@ -24,6 +24,7 @@ namespace Cats.Areas.Procurement.Controllers
         {
            // var bidWinner = _bidWinnerService.Get(m => m.Transporter.Name.StartsWith(transporter));
             var transporterOrder = _transportOrderService.Get(m => m.Transporter.Name.StartsWith(transporter));
+            ViewData["Transporters"] = transporterOrder;
             return View(transporterOrder);
         }
         
