5b04a61e9875908149fe53570e35159b7c017054
mikedebela <johntech82@gmail.com>
2013-07-22 17:39:33 +0100

    Importing Roles, Tasks, Operations from .NetSqlAzMan

---
+++ Models/Cats.Models.Security/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models.Security
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public byte[] DBUserSid { get; set; }
+    }
+}
