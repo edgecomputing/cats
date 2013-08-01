2379eea392373baa097f5e8dfd26a88133f2d44d
Banty <bantyder@gmail.com>
2013-08-01 12:07:44 +0300

    Database change required by ration

--- Migrations/Cats.DatabaseSchema/Logistics/Tables/Ration.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [Logistics].[Ration] (
-    [RationID]    INT          IDENTITY (1, 1) NOT NULL,
-    [CommodityID] INT          NOT NULL,
-    [Amount]      DECIMAL (18) NOT NULL,
-    CONSTRAINT [PK_Ration] PRIMARY KEY CLUSTERED ([RationID] ASC)
-);
-
