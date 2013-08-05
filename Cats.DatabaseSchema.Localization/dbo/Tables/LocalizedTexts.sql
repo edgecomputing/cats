628ddaf35566d954db82729f9153ad6a49b76447
Beka Mengistu <bekalive@gmail.com>
2013-08-05 20:47:16 +0200

    Updated Database schema for PSNP plan Added database project for localization data

---
+++ Cats.DatabaseSchema.Localization/dbo/Tables/LocalizedTexts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LocalizedTexts] (
+    [LocalizedTextId] INT            IDENTITY (1, 1) NOT NULL,
+    [LanguageCode]    NVARCHAR (10)  NULL,
+    [TextKey]         NVARCHAR (200) NULL,
+    [Value]           NVARCHAR (200) NULL
+);
+
