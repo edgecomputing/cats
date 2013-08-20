32a862aab1af4e770d3d08a139e5f700f8ae48dc
Beka Mengistu <bekalive@gmail.com>
2013-08-20 15:46:14 +0300

    View enhancement-Workflow

---
+++ Migrations/Cats.DatabaseSchema/dbo/Tables/StateTemplate.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StateTemplate] (
+    [StateTemplateID]         INT           IDENTITY (1, 1) NOT NULL,
+    [ParentProcessTemplateID] INT           NULL,
+    [Name]                    NVARCHAR (50) NULL,
+    [AllowedAccessLevel]      INT           NULL,
+    [StateNo]                 INT           NOT NULL,
+    [StateType]               INT           NULL
+);
+
