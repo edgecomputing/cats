6cf962008eb264986b80c6887ab2fa65933aedc2
Fisseha Abebe <abebefisseha5@gmail.com>
2013-08-28 11:18:27 +0300

    Added Contribuiton Controller Test

---
+++ Tests/Cats.Tests/ControllersTests/ContributionControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cats.Areas.EarlyWarning.Controllers;
+using Cats.Models;
+using Cats.Services.EarlyWarning;
+using Moq;
+using NUnit.Framework;
+
+namespace Cats.Tests.ControllersTests
+{
+    public class ContributionControllerTest
+    {
+        private readonly ContributionController _contributionController;
+
+#region Setup
+        [SetUp]
+        public void Init()
+        {
+            var contribution = new List<Contribution>
+                {
+                    new Contribution {ContributionID = 1,HRDID = 1,DonorID = 1,Year = 2012},
+                    new Contribution {ContributionID = 2,HRDID = 2,DonorID = 1,Year = 2012}
+
+                };
+
+
+            var contributionService = new Mock<IContributionService>();
+            contributionService.Setup(m => m.GetAllContribution()).Returns(contribution);
+
+
+            var contributionDetail = new List<ContributionDetail>()
+                {
+                    new ContributionDetail
+                        {
+                            ContributionDetailID = 1,
+                            ContributiionID = 1,
+                            CommodityID = 1,
+                            PledgeDate = new DateTime(12/12/12),
+                            PledgeReferenceNo = "ADE123",
+                            Quantity = 100
+                        },
+
+                         new ContributionDetail
+                        {
+                            ContributionDetailID = 2,
+                            ContributiionID = 1,
+                            CommodityID = 2,
+                            PledgeDate = new DateTime(12/12/12),
+                            PledgeReferenceNo = "ADE123",
+                            Quantity = 100
+                        }
+                };
+
+            var contributionDetailService = new Mock<IContributionDetailService>();
+            contributionDetailService.Setup(m => m.GetAllContributionDetail()).Returns(contributionDetail);
+        }
+#endregion
+
+
+    }
+}
--- Web/Areas/EarlyWarning/Controllers/HRDController.cs
+++ Web/Areas/EarlyWarning/Controllers/HRDController.cs
@@ -153,38 +153,7 @@ namespace Cats.Areas.EarlyWarning.Controllers
 
         public ActionResult RegionalSummary(int id=0)
         {
-            //var details = _hrdDetailService.Get(hrdDetail => hrdDetail.HRDID == id);
-            //var hrd = _hrdService.FindById(id);
-            ////details.First().HRD;
-            //var cerealCoefficient = hrd.Ration.RationDetails.First(m => m.Commodity.CommodityID == 1).Amount;
-            //var blendFoodCoefficient = hrd.Ration.RationDetails.First(m => m.Commodity.CommodityID == 2).Amount;
-            //var pulseCoefficient = hrd.Ration.RationDetails.First(m => m.Commodity.CommodityID == 3).Amount;
-            //var oilCoefficient = hrd.Ration.RationDetails.First(m => m.Commodity.CommodityID == 4).Amount;
-
-            //ViewBag.SeasonID = hrd.Season.Name;
-            //ViewBag.Year = hrd.Year;
-
-            //var groupedTotal = from detail in details
-            //                   group detail by detail.AdminUnit.AdminUnit2.AdminUnit2 into regionalDetail
-            //                   select new
-            //                       {
-            //                           Region = regionalDetail.Key,
-            //                           NumberOfBeneficiaries = regionalDetail.Sum(m => m.NumberOfBeneficiaries),
-            //                           Duration = regionalDetail.Sum(m => m.DurationOfAssistance)
-            //                       };
-
-            //var viewModel = from total in groupedTotal
-            //                select new RegionalSummaryViewModel
-            //                    {
-            //                        RegionName = total.Region.Name,
-            //                        NumberOfBeneficiaries = total.NumberOfBeneficiaries,
-            //                        Cereal = cerealCoefficient * total.NumberOfBeneficiaries * total.Duration,
-            //                        BlededFood = blendFoodCoefficient * total.NumberOfBeneficiaries * total.Duration,
-            //                        Oil = oilCoefficient * total.NumberOfBeneficiaries * total.Duration,
-            //                        Pulse = pulseCoefficient * total.NumberOfBeneficiaries * total.Duration
-            //                    };
-            ////ViewData["viewModel"] = viewModel;
-            //var summary = viewModel.ToList();
+            
             var hrd = _hrdService.Get(m => m.HRDID == id).FirstOrDefault();
             ViewBag.SeasonID = hrd.Season.Name;
             ViewBag.Year = hrd.Year;
@@ -258,8 +227,8 @@ namespace Cats.Areas.EarlyWarning.Controllers
             // hrd.HRDDetails = new List<HRDDetail>();
             ViewBag.Year = DateTime.Today.Year;
             ViewBag.RationID = new SelectList(_rationService.GetAllRation(), "RationID", "RefrenceNumber", hrd.RationID = 1);
-            //ViewBag.NeedAssessmentID = new SelectList(_needAssessmentService.GetAllNeedAssessmentHeader().Where(m => m.NeedAssessment.NeedAApproved == true), "NAHeaderId",
-            //                                          "NeedACreatedDate");
+            ViewBag.NeedAssessmentID = new SelectList(_needAssessmentService.GetAllNeedAssessmentHeader().Where(m => m.NeedAssessment.NeedAApproved == true), "NAHeaderId",
+                                                      "NeedACreatedDate");
 
             ViewData["SeasonID"] = new SelectList(_seasonService.GetAllSeason(), "SeasonID", "Name");
             var woredas = _adminUnitService.FindBy(m => m.AdminUnitTypeID == 3);
