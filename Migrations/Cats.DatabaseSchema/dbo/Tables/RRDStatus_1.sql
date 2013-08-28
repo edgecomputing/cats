50f453854a2940697aadb63de6f3884fa6c0770a
nathnael <natash1145@gmail.com>
2013-08-28 12:26:39 +0300

    Adding breadcrumb and generic buttons to all views

---
+++ Migrations/Cats.DatabaseSchema/dbo/Tables/RRDStatus_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[RRDStatus] (
+    [RRDStatusID] INT          NOT NULL,
+    [Status]      VARCHAR (50) NULL,
+    CONSTRAINT [PK_RRDStatus] PRIMARY KEY CLUSTERED ([RRDStatusID] ASC)
+);
+
--- Migrations/Cats.DatabaseSchema/dbo/Tables/RationDetail_1.sql
+++ Migrations/Cats.DatabaseSchema/dbo/Tables/RationDetail_1.sql
@@ -2,13 +2,14 @@
     [RationDetailID] INT             IDENTITY (1, 1) NOT NULL,
     [RationID]       INT             NOT NULL,
     [CommodityID]    INT             NOT NULL,
-    [Amount]         DECIMAL (18, 2) NOT NULL,
+    [Amount]         DECIMAL (18, 6) NOT NULL,
     [UnitID]         INT             NULL,
     CONSTRAINT [PK_Ration] PRIMARY KEY CLUSTERED ([RationDetailID] ASC),
     CONSTRAINT [FK_RationDetail_Commodity] FOREIGN KEY ([CommodityID]) REFERENCES [dbo].[Commodity] ([CommodityID]),
-    CONSTRAINT [FK_RationDetail_Ration] FOREIGN KEY ([RationID]) REFERENCES [dbo].[Ration] ([RationID]),
-    CONSTRAINT [FK_RationDetail_Unit] FOREIGN KEY ([UnitID]) REFERENCES [dbo].[Unit] ([UnitID])
+    CONSTRAINT [FK_RationDetail_Ration] FOREIGN KEY ([RationID]) REFERENCES [dbo].[Ration] ([RationID])
 );
+
+
 
 
 
