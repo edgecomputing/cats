4eef5bb52476ade22438527899e1e7afcb2f4d6c
banty <bantyder@gmail.com>
2013-08-29 11:25:45 +0300

    CATS-575 Create Model for PSNP & HRD  plan integration with request

---
+++ Models/Cats.Models/HRDPSNPPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models
+{
+   public class HRDPSNPPlan
+    {
+       public int Year { get; set; }
+       public int ProgramID      { get; set; }
+       public int Month { get; set; }
+       public int RegionID { get; set; }
+      
+    }
+}
