b2c9d8faee6683c2361d29e08a120c0e07e94470
Beka Mengistu <bekalive@gmail.com>
2013-09-02 09:33:58 +0300

    Dashboard Customization

---
+++ Models/Cats.Models/UIWidget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models
+{
+    public class UIWidget
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Source { get; set; }
+
+    }
+}
