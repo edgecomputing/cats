0ebcf16a110f77543964dea984f5d95eaac5f874
dawit <dawitdb@yahoo.com>
2013-09-02 17:58:37 +0300

    sync lettertemplateModel_

---
+++ Migrations/Cats.DatabaseSchema/dbo/Tables/LetterTemplate_.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LetterTemplate_] (
+    [LetterTemplateID] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (500) NULL,
+    [FileName]         NVARCHAR (500) NULL,
+    CONSTRAINT [PK_LetterTemplate2] PRIMARY KEY CLUSTERED ([LetterTemplateID] ASC)
+);
+
