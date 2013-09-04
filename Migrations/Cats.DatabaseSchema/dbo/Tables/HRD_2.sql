4f33d9fd148f4c250bdf0af0690baa0bcbed2153
Fisseha Abebe <abebefisseha5@gmail.com>
2013-09-04 14:49:01 +0300

    Added Unique Key for Season and Year and sync the table

--- Migrations/Cats.DatabaseSchema/dbo/Tables/HRD_2.sql
+++ Migrations/Cats.DatabaseSchema/dbo/Tables/HRD_2.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[HRD] (
     [HRDID]          INT      IDENTITY (1, 1) NOT NULL,
-    [Year]           INT      NULL,
-    [CreatedBy]      INT      NULL,
+    [Year]           INT      NOT NULL,
+    [CreatedBy]      INT      NOT NULL,
     [RevisionNumber] INT      NULL,
-    [DateCreated]    DATETIME NULL,
-    [SeasonID]       INT      NULL,
+    [DateCreated]    DATETIME NOT NULL,
+    [SeasonID]       INT      NOT NULL,
     [PublishedDate]  DATETIME NULL,
-    [RationID]       INT      NULL,
-    [Status]         INT      NULL,
+    [RationID]       INT      NOT NULL,
+    [Status]         INT      NOT NULL,
     CONSTRAINT [PK_HRD] PRIMARY KEY CLUSTERED ([HRDID] ASC)
 );
 
@@ -16,4 +16,11 @@
 
 
 
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_HRD]
+    ON [dbo].[HRD]([SeasonID] ASC, [Year] ASC);
 
