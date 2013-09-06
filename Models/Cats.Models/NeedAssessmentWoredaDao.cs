f084d318e9f34748588c1129fd7c1478ec06a5c1
dawit <dawitdb@yahoo.com>
2013-09-06 14:46:33 +0300

    add data annotations to need assessment model

--- Models/Cats.Models/NeedAssessmentWoredaDao.cs
+++ Models/Cats.Models/NeedAssessmentWoredaDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,18 +16,53 @@ namespace Cats.Models
         public int NeedAID  { get; set; }
 
         public int NAId { get; set; }
+        [Range(0, Int32.MaxValue)]
+        [DataType(DataType.Currency)]
         public Nullable<int> ProjectedMale { get; set; }
+         [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> ProjectedFemale { get; set; }
+         [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> RegularPSNP { get; set; }
+         [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> PSNP { get; set; }
+
+         [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> NonPSNP { get; set; }
+         
+        [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> Contingencybudget { get; set; }
+
+         [DataType(DataType.Currency)]
+         [Range(0, Int32.MaxValue)]
         public Nullable<int> TotalBeneficiaries { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> PSNPFromWoredasMale { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> PSNPFromWoredasFemale { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> PSNPFromWoredasDOA { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> NonPSNPFromWoredasMale { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> NonPSNPFromWoredasFemale { get; set; }
+
+         [DataType(DataType.Currency)]
+        [Range(0, Int32.MaxValue)]
         public Nullable<int> NonPSNPFromWoredasDOA { get; set; }
         
                                     
