c670e1c9ef6406ef9320d43d48466177ea36a43e
robi-github <blessedethio@gmail.com>
2013-09-06 16:41:26 +0300

    Fix missing links on the setting dashboard

--- Web/Controllers/DashboardController.cs
+++ Web/Controllers/DashboardController.cs
@@ -28,10 +28,23 @@ namespace Cats.Controllers
             return PartialView("_Requests", model);
         }
 
+        public JsonResult dRegionalMonthlyRequests()
+        {
+           return Json(_IDashboardService.Requests(),JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult rr() {
+            return Json(_IDashboardService.Requests(),JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Requests()
         {
             var model = _IDashboardService.Requests();
             return PartialView("_Requests", model);
+        }
+
+        public JsonResult Re() {
+            return Json(_IDashboardService.RMRequests(), JsonRequestBehavior.AllowGet);
         }
         
         public ActionResult RegionalRequestsById(int RegionId)
@@ -49,11 +62,11 @@ namespace Cats.Controllers
             return Json(_IDashboardService.BarNoOfBeneficiaries(), JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult BarNeedAssessment(string regionName="Dire Dawa") {
+        public JsonResult BarNeedAssessment(string regionName) {
             return Json(_INeedAssessmentSummaryService.NeedAssessmentByRegion(regionName), JsonRequestBehavior.AllowGet);
         }
         
-       // int y = DateTime.Now.Year;
+        // int y = DateTime.Now.Year;
 
         public JsonResult BarNeedAssessmentbY(int year = 2013)
         {
