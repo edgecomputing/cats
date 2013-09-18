15499ae6711d7d85f6a7013ebb0a2dbe567417d4
banty <bantyder@gmail.com>
2013-09-18 16:37:16 +0300

    Cats-688 CommodityType

--- Cats.Web.Adminstration/Infrastructure/NinjectDependencyResolver.cs
+++ Cats.Web.Adminstration/Infrastructure/NinjectDependencyResolver.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
-using Cats.Data.UnitWork;
 using Cats.Services.Security;
 using LanguageHelpers.Localization.Services;
 using Ninject;
 using log4net;
+using Cats.Services.Administration;
 
 
 namespace Cats.Web.Administration.Infrastructure
@@ -34,13 +34,17 @@ namespace Cats.Web.Administration.Infrastructure
         
         private void AddBindings()
         {
-            kernel.Bind<IUserAccountService>().To<UserAccountService>();
-            kernel.Bind<Cats.Data.Security.IUnitOfWork>().To<Cats.Data.Security.UnitOfWork>();
-            kernel.Bind<IUnitOfWork>().To<IUnitOfWork>();
-            kernel.Bind<ILocalizedTextService>().To<LocalizedTextService>();
+            kernel.Bind<Data.UnitWork.IUnitOfWork>().To<Data.UnitWork.IUnitOfWork>();
+            kernel.Bind<Data.Security.IUnitOfWork>().To<Data.Security.UnitOfWork>();
             kernel.Bind<LanguageHelpers.Localization.Data.IUnitOfWork>().To<LanguageHelpers.Localization.Data.UnitOfWork>();
+          
+            kernel.Bind<IUserAccountService>().To<UserAccountService>();
+            kernel.Bind<ILocalizedTextService>().To<LocalizedTextService>();
             kernel.Bind<ILog>().ToMethod(context => LogManager.GetLogger(context.Request.Target.Member.DeclaringType));
             kernel.Bind<ILanguageService>().To<LanguageService>();
+            kernel.Bind<ICommodityTypeService>().To<CommodityTypeService>();
+
+
 
         }
     }
