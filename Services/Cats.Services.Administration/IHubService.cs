29511b4a4638f5cf4bcc66580c336ba62d54c003
Fisseha Abebe <abebefisseha5@gmail.com>
2013-09-20 16:41:35 +0300

    Added Hub Service

---
+++ Services/Cats.Services.Administration/IHubService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Cats.Models;
+using System.Threading.Tasks;
+
+namespace Cats.Services.Administration
+{
+   public interface IHubService:IDisposable
+    {
+        bool AddHub(Hub hub);
+        bool DeleteHub(Hub hub);
+        bool DeleteById(int id);
+        bool EditHub(Hub hub);
+        Hub FindById(int id);
+        List<Hub> GetAllHub();
+        List<Hub> FindBy(Expression<Func<Hub, bool>> predicate);
+    }
+}
