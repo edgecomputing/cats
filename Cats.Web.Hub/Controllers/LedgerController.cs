de5476ce157fcaf8a214663bda6c96130f412661
robi-github <blessedethio@gmail.com>
2013-09-23 09:50:24 +0300

    add Ledger controller

---
+++ Cats.Web.Hub/Controllers/LedgerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cats.Web.Hub.Controllers
+{
+    public class LedgerController : Controller
+    {
+        //
+        // GET: /Ledger/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit() {
+
+            return View();
+        }
+
+        public ActionResult Delete() {
+
+            return View();
+        }
+
+        public ActionResult Add() {
+            return View();
+        }
+
+    }
+}
