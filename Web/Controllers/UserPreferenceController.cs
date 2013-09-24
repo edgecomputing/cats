bdf5a0707b9d064843fb1862b3126ea346114d49
mikedebela <johntech82@gmail.com>
2013-09-24 12:08:56 +0100

    Edit User Preference

--- Web/Controllers/UserPreferenceController.cs
+++ Web/Controllers/UserPreferenceController.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Cats.Services.Security;
-using Cats.Data.Security;
 using Cats.Areas.Settings.Models;
+using Cats.Services.Security;
 
-namespace Cats.Areas.Settings.Controllers
+namespace Cats.Controllers
 {
     public class UserPreferenceController : Controller
     {
-        //
-        // GET: /Settings/UserPreference/
 
         private readonly IUserAccountService userService;
 
@@ -21,53 +18,22 @@ namespace Cats.Areas.Settings.Controllers
             this.userService = _userService;
         }
 
-        public ActionResult UserPreference()
+        public ActionResult Edit(UserPreferenceEditModel model)
         {
-            var user = userService.GetUserDetail(HttpContext.User.Identity.Name);
-            var userPreference = new UserPreferenceViewModel
+            if (ModelState.IsValid)
             {
-                Language = new List<LanguageCode> { new LanguageCode { Language = user.LanguageCode } },
-                KeyboardLanguage = new List<Keyboard> { new Keyboard { KeyboardLanguage = user.Keyboard } },
-                PreferedWeightMeasurement = new List<PreferedWeightMeasurementUnit> { new PreferedWeightMeasurementUnit { PreferedWeightMeasurement = user.PreferedWeightMeasurment } },
-                DatePreference = new List<Calendar> { new Calendar { DatePreference = user.DatePreference } },
-                DefaultTheme = new List<Theme> { new Theme { DefaultTheme = user.DefaultTheme } }
-            };     
-      
-            var userPreferences = userService.GetUserPreferences();
-            UserPreferenceViewModel userPreferencesModel = new UserPreferenceViewModel
-            {
-                Language = new List<LanguageCode>(),
-                KeyboardLanguage = new List<Keyboard>(),
-                PreferedWeightMeasurement = new List<PreferedWeightMeasurementUnit>(),
-                DatePreference = new List<Calendar>(),
-                DefaultTheme = new List<Theme>()
-            };
-            foreach (var preferences in userPreferences)
-            {
-                userPreferencesModel.Language.Add(new LanguageCode { Language = preferences.LanguageCode });
-                userPreferencesModel.KeyboardLanguage.Add(new Keyboard { KeyboardLanguage = preferences.Keyboard });
-                userPreferencesModel.PreferedWeightMeasurement.Add(new PreferedWeightMeasurementUnit { PreferedWeightMeasurement = preferences.PreferedWeightMeasurment });
-                userPreferencesModel.DatePreference.Add(new Calendar { DatePreference = preferences.DatePreference });
-                userPreferencesModel.DefaultTheme.Add(new Theme { DefaultTheme = preferences.DefaultTheme });
+                var user = userService.GetUserDetail(HttpContext.User.Identity.Name);
+                user.DefaultTheme = model.DefaultTheme;
+                user.DatePreference = model.DatePreference;
+                user.Keyboard = model.KeyboardLanguage;
+                user.LanguageCode = model.Language;
+                user.PreferedWeightMeasurment = model.PreferedWeightMeasurement;
+
+                // Edit user preference
+                ModelState.AddModelError("Success", "Preference Updated");
+                userService.Save(user);
             }
-            userPreferencesModel.CurrentPreference = userPreference;
-            return View(userPreferencesModel);
+            return RedirectToAction("Preference", "Home");
         }
-
-        public ActionResult Edit(UserPreferenceViewModel model)
-        {
-            var user = userService.GetUserDetail(HttpContext.User.Identity.Name);
-            user.DefaultTheme = model.DefaultTheme.First().DefaultTheme;
-            user.DatePreference = model.DatePreference.First().DatePreference;
-            user.Keyboard = model.KeyboardLanguage.First().KeyboardLanguage;
-            user.LanguageCode = model.Language.First().Language;
-            user.PreferedWeightMeasurment = model.PreferedWeightMeasurement.First().PreferedWeightMeasurement;
-
-            // Edit user preference
-            ModelState.AddModelError("Success", "Update Successful");
-            userService.Save(user);
-            return View(model);
-        }
-
     }
 }
