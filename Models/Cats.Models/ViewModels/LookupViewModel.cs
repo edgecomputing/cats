7af050bb6b7ccf47f8fb05afed50de1fdcae6167
nathnael <natash1145@gmail.com>
2013-10-03 17:13:18 +0300

    CIT-69 The whole general preference was fixed.

---
+++ Models/Cats.Models/ViewModels/LookupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cats.Models.ViewModels
+{
+    public class LookupViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StringID { get; set; }
+    }
+}
