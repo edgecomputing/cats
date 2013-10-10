39224c4e45719f848019f5e7771c0c9494b814e0
robi-github <blessedethio@gmail.com>
2013-10-10 16:22:58 +0300

    fix CIT-94 disabled stores combobox

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace Cats.Controllers
 
         //
         // GET: /Home/
-        [Authorize]
+   //     [Authorize]
         public ActionResult Index(int regionId=4)
         {
             //var req = _reliefRequistionService.FindBy(t => t.RegionID == regionId);
