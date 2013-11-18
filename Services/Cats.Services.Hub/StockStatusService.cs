240166aa41262f62a606359166b823c32a929bb5
robi-github <blessedethio@gmail.com>
2013-11-18 17:35:28 +0300

    Format to decimal places values on stock status

--- Services/Cats.Services.Hub/StockStatusService.cs
+++ Services/Cats.Services.Hub/StockStatusService.cs
@@ -255,7 +255,6 @@ namespace Cats.Services.Hub
         public List<HubFreeStockSummaryView> GetStockSummaryD(int program, DateTime date)
         {
             var status = _transactionService.Get(t => t.HubID != null && t.ProgramID == program && DateTime.Compare(t.TransactionDate, date) <= 0);
-
             var grouped = (
                            from s in status
                            group s by s.HubID into g
@@ -285,7 +284,6 @@ namespace Cats.Services.Hub
                         free = free + Math.Abs(s.QuantityInMT);
                     }
                 }
-
                 var item = new HubFreeStockSummaryView()
                 {
                     HubName = hubName,
