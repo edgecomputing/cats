b1260736652375536a7ac692546368bceeaa1bba
Banty <bantyder@gamil.com>
2013-12-09 15:07:04 +0300

    Dispatch from dispatch allocation CATS-827

--- Models/Cats.Models.Hub/ViewModels/Dispatch/DispatchViewModel.cs
+++ Models/Cats.Models.Hub/ViewModels/Dispatch/DispatchViewModel.cs
@@ -671,4 +671,121 @@ namespace Cats.Models.Hubs
 
         public bool ChangeStoreManPermanently { get; set; }
     }
+
+    public class DispatchViewModel
+    {
+
+
+        public Guid? DispatchID { get; set; }
+
+
+        public string FDP { get; set; }
+        public string Region { get; set; }
+        public string Zone { get; set; }
+        public string Woreda { get; set; }
+
+        [Required]
+        [Display(Name = "Store Man")]
+        [UIHint("AmharicTextBox")]
+        public string DispatchedByStoreMan { get; set; }
+
+        [Required]
+        [Display(Name = "Dispatch Date")]
+        public DateTime DispatchDate { get; set; }
+
+        public string RequisitionNo { get; set; }
+
+        [Required]
+        [Display(Name = "GIN")]
+        [RegularExpression("[0-9]*", ErrorMessage = "Only numbers allowed for GIN")]
+        [Remote("NotUnique", "Dispatch", AdditionalFields = "DispatchID")]
+        [StringLength(7, ErrorMessage = "Length Must be less than or equal to 7")]
+        public string GIN { get; set; }
+
+        public string BidNumber { get; set; }
+
+
+
+        //[RegularExpression("[a-zA-Z\\s]*", ErrorMessage = "Only letters are allowed for Driver name")]
+        [Required(ErrorMessage = "Name of the driver taking the commodities is a required field.")]
+        [Display(Name = "Received By (Driver Name)")]
+        [UIHint("AmharicTextBox")]
+        public string DriverName { get; set; }
+
+
+        [Required(ErrorMessage = "Prime Plate Number is a required field")]
+        [Display(Name = "Plate No. Prime")]
+        public string PlateNo_Prime { get; set; }
+
+
+        [Display(Name = "Plate No. Trailer")]
+        public string PlateNo_Trailer { get; set; }
+
+
+        public string Transporter { get; set; }
+
+
+        public string ProjectNumber { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+
+        public int Program { get; set; }
+
+
+        [Display(Name = "Weigh Bridge Ticket Number")]
+        public string WeighBridgeTicketNumber { get; set; }
+
+
+        public int Round { get; set; }
+
+
+
+        public string SINumber { get; set; }
+
+        public int CommodityID { get; set; }
+        public string Commodity { get; set; }
+        [Display(Name = "Remark")]
+        [UIHint("AmharicTextArea")]
+        public string Remark { get; set; }
+
+        public int Type { get; set; }
+        public decimal QuantityInUnit { get; set; }
+        public decimal Quantity { get; set; }
+        [Display(Name = "Unit")]
+        public int UnitID { get; set; }
+
+        public Guid DispatchAllocationID { get; set; }
+        public int TransporterID { get; set; }
+
+        public int ProgramID
+        {
+            get;
+            set;
+        }
+
+        public int HubID
+        {
+            get;
+            set;
+        }
+
+        public int FDPID
+        {
+            get;
+            set;
+        }
+
+        public DateTime CreatedDate
+        {
+            get;
+            set;
+        }
+
+        public int UserProfileID { get; set; }
+        public int? ShippingInstructionID { get; set; }
+        public int? ProjectCodeID { get; set; }
+    }
 }
