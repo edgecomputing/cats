6c619100612a9445f5211516dfe8c61bb7a7d83b
Fisseha Abebe <abebefisseha5@gmail.com>
2013-12-24 14:47:53 +0300

    CATS-855 displayed necessary information on bid admin view

--- Web/Areas/Procurement/Controllers/BidAdministrationController.cs
+++ Web/Areas/Procurement/Controllers/BidAdministrationController.cs
@@ -22,15 +22,21 @@ namespace Cats.Areas.Procurement.Controllers
         private readonly IApplicationSettingService _applicationSettingService;
         private readonly IWorkflowStatusService _workflowStatusService;
         private readonly IUserAccountService _userAccountService;
+        private readonly IBidService _bidService;
+        private readonly ITransportBidPlanService _bidPlanService;
        
         public BidAdministrationController(IBidWinnerService bidWinnerService,IApplicationSettingService applicationSettingService,
-                                           IWorkflowStatusService workflowStatusService,IUserAccountService userAccountService)
+                                           IWorkflowStatusService workflowStatusService,IUserAccountService userAccountService,
+                                           IBidService bidService,ITransportBidPlanService bidPlanService)
+                                  
                                           
         {
             _bidWinnerService = bidWinnerService;
             _applicationSettingService = applicationSettingService;
             _workflowStatusService = workflowStatusService;
             _userAccountService = userAccountService;
+            _bidService = bidService;
+            _bidPlanService = bidPlanService;
 
         }
         // GET: /Procurement/BidAdministration/
@@ -44,10 +50,12 @@ namespace Cats.Areas.Procurement.Controllers
             }
             ViewBag.BidID=currentBid.SettingValue;
             ViewBag.BidAdminStatus = id;
-            ViewBag.RFQGenerated = _bidWinnerService.IsRfqGenerated(int.Parse(currentBid.SettingValue));
-            var bidWibnnerViewModel = GetListOfBidWinners(int.Parse(currentBid.SettingValue));
+            //ViewBag.RFQGenerated = _bidWinnerService.IsRfqGenerated(int.Parse(currentBid.SettingValue));
+            var bidWinnerViewModel = GetListOfBidWinners(int.Parse(currentBid.SettingValue));
+            if (bidWinnerViewModel == null || !bidWinnerViewModel.Bidwinners.Any())
+                return RedirectToAction("WithoutRFQ","BidAdministration");
            
-           return View(bidWibnnerViewModel);
+           return View(bidWinnerViewModel);
          
         }
 
@@ -66,7 +74,12 @@ namespace Cats.Areas.Procurement.Controllers
             var bidsToDisplay = GetBidWinners(bids).ToList();
             return Json(bidsToDisplay.ToDataSourceResult(request));
         }
-
+        public ActionResult BidPlanDetail_Read([DataSourceRequest] DataSourceRequest request,int id=0)
+        {
+            var bidPlan = _bidPlanService.FindById(id);
+            var planDetailsToDisplay = GetBidPlanDetail(bidPlan.TransportBidPlanID).ToList();
+            return Json(planDetailsToDisplay.ToDataSourceResult(request));
+        }
         private IEnumerable<BidWinnerViewModel> GetBidWinners(IEnumerable<BidWinner> bidWinners)
         { 
              var datePref = _userAccountService.GetUserInfo(HttpContext.User.Identity.Name).DatePreference;
@@ -111,6 +124,46 @@ namespace Cats.Areas.Procurement.Controllers
                           DestinationID = selectedWinner.DestinationId
                       });
       }
+    public ActionResult WithoutRFQ()
+    {
+        var currentBid = _applicationSettingService.FindBy(t => t.SettingName == "CurrentBid").FirstOrDefault();
+        if (currentBid == null)
+        {
+            return HttpNotFound();
+        }
+        var bid = _bidService.FindById(int.Parse(currentBid.SettingValue));
+        ViewBag.BidNumber = bid.BidNumber;
+        //ViewBag.BidPlanID = bid.TransportBidPlanID;
+        var bidPlanDetailViewModel = new BidPlanDetailListViewModel()
+            {
+                BidPlanID = bid.TransportBidPlanID,
+                BidPlanDetails = GetBidPlanDetail(bid.TransportBidPlanID)
+            };
+            
+        return View(bidPlanDetailViewModel);
+    }
+    private IEnumerable<BidPlanDetailViewModel> GetBidPlanDetail(int planID)
+        {
+            var bidPlan = _bidPlanService.FindById(planID);
+            if(bidPlan!=null)
+            {
+                return (from bidplanDetail in bidPlan.TransportBidPlanDetails
+                        select new BidPlanDetailViewModel()
+                            {
+                                BidPlanID=bidPlan.TransportBidPlanID,
+                                BidPlanDetailID = bidplanDetail.TransportBidPlanDetailID,
+                                SourceID = bidplanDetail.SourceID,
+                                Warehouse = bidplanDetail.Source.Name,
+                                DestinationID = bidplanDetail.DestinationID,
+                                Woreda = bidplanDetail.Destination.Name,
+                                Region=bidplanDetail.Destination.AdminUnit2.AdminUnit2.Name,
+                                Quantity = bidplanDetail.Quantity,
+                                ProgramID = bidplanDetail.ProgramID,
+                                Program = bidplanDetail.Program.Name
+                            });
+            }
+        return null;
+        }
     
     }
 }
