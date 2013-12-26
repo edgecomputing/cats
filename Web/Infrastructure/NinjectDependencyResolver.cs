26452b1dbdd1f42c46693a363071bcd59367b26c
Beka Mengistu <bekalive@gmail.com>
2013-12-26 17:40:33 +0300

    SMS Messaging

--- Web/Infrastructure/NinjectDependencyResolver.cs
+++ Web/Infrastructure/NinjectDependencyResolver.cs
@@ -63,6 +63,8 @@ namespace Cats.Infrastructure
         
         private void AddBindings()
         {
+
+            kernel.Bind<IMessagingService>().To<SMSMessagingService>();
             kernel.Bind<IPaymentRequestService>().To<PaymentRequestService>();
             kernel.Bind<ISIPCAllocationService>().To<SIPCAllocationService>();
             kernel.Bind<IPromisedContributionService>().To<PromisedContributionService>();
