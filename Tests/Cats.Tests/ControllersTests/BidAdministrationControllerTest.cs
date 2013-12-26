a253b7dd9be76fa541a24e811899da88eed92c1e
Fisseha Abebe <abebefisseha5@gmail.com>
2013-12-26 13:49:57 +0300

    FIx Test problem

--- Tests/Cats.Tests/ControllersTests/BidAdministrationControllerTest.cs
+++ Tests/Cats.Tests/ControllersTests/BidAdministrationControllerTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Cats.Areas.Procurement.Controllers;
@@ -54,11 +51,11 @@ namespace Cats.Tests.ControllersTests
 
             var applicationSetting = new List<ApplicationSetting>
             {  
-                new ApplicationSetting()
+                new ApplicationSetting
                 {
                    SettingID = 1,
                    SettingName = "CurrentBid",
-                  SettingValue = "12"
+                  SettingValue = "1"
                 }
 
             };
@@ -88,7 +85,7 @@ namespace Cats.Tests.ControllersTests
             var workFlowStatusService = new Mock<IWorkflowStatusService>();
 
             var userAccountService = new Mock<IUserAccountService>();
-            userAccountService.Setup(t => t.GetUserInfo(It.IsAny<string>())).Returns(new UserInfo()
+            userAccountService.Setup(t => t.GetUserInfo(It.IsAny<string>())).Returns(new UserInfo
             {
                 UserName = "user",
                 DatePreference = "en"
@@ -115,7 +112,7 @@ namespace Cats.Tests.ControllersTests
             };
 
             var bidService = new Mock<IBidService>();
-            bidService.Setup(m => m.FindById(1)).Returns(new Bid()
+            bidService.Setup(m => m.FindById(1)).Returns(new Bid
                 {
                     BidID = 1,
                     StartDate = new DateTime(12 / 12 / 2004),
@@ -130,24 +127,33 @@ namespace Cats.Tests.ControllersTests
                         Year = 2012,
                         YearHalf = 1,
                         ProgramID = 1,
-                        TransportBidPlanDetails = new List<TransportBidPlanDetail>()
+                        Program = new Program { ProgramID = 1, Name = "Relief" },
+                        TransportBidPlanDetails = new List<TransportBidPlanDetail>
                             {
                                 new TransportBidPlanDetail
                                     {
                                         TransportBidPlanDetailID = 1,
                                         BidPlanID = 1,
+                                        BidPlan = new TransportBidPlan {TransportBidPlanID = 1,ProgramID = 1,Year = 2012,YearHalf = 2},
                                         ProgramID = 1,
+                                        Program = new Program {ProgramID = 1,Name = "Relief"},
                                         SourceID = 25,
+                                        Source = new Hub {HubID = 1,Name = "Adama"},
                                         DestinationID = 2,
+                                        Destination = new AdminUnit {AdminUnitID = 1,Name = "Admin Unit Name"},
                                         Quantity = 123
                                     },
                                 new TransportBidPlanDetail
                                     {
                                         TransportBidPlanDetailID = 2,
                                         BidPlanID = 1,
+                                        BidPlan = new TransportBidPlan {TransportBidPlanID = 2,ProgramID = 2,Year = 2012,YearHalf = 2},
                                         ProgramID = 1,
+                                        Program = new Program {ProgramID = 1,Name = "PSNP"},
                                         SourceID = 24,
+                                        Source = new Hub {HubID = 2,Name = "Kombolecha"},
                                         DestinationID = 3,
+                                        Destination = new AdminUnit {AdminUnitID = 3,Name = "Admin Unit Name"},
                                         Quantity = 100
                                     }
                             }
@@ -177,22 +183,22 @@ namespace Cats.Tests.ControllersTests
 
         #endregion
 
-        //[Test]
-        //public void CanShowBidsWithoutRfq()
-        //{
-        //    var result = _bidAdministrationController.WithoutRFQ();
+        [Test]
+        public void CanShowBidsWithoutRfq()
+        {
+            var result = _bidAdministrationController.WithoutRFQ();
 
-        //    Assert.IsNotNull(result);
-        //}
-        //[Test]
-        //public void CanShowIndexView()
-        //{
-        //    var result = _bidAdministrationController.Index(1);
-        //    //Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
+        }
+        [Test]
+        public void CanShowIndexView()
+        {
+            var result = _bidAdministrationController.Index(1);
+            Assert.IsNotNull(result);
 
-        //    Assert.IsInstanceOf<ViewResult>(result);
-        //    Assert.IsInstanceOf<SelectedBidWinnerViewModel>(((ViewResult)result).Model);
-        //}
+            //Assert.IsInstanceOf<ViewResult>(result);
+            //Assert.IsInstanceOf<SelectedBidWinnerViewModel>(((ViewResult)result).Model);
+        }
         //[Test]
         //public void CanReadBidPlanDetail()
         //{
