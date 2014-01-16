a521d0ca4defe1ad396bd5a37ea1f6290a633586
robi <blessedethio@gmail.com>
2014-01-16 15:12:22 +0300

    Add an send method that uses external url(php)

--- Web/Controllers/SMSGatewayController.cs
+++ Web/Controllers/SMSGatewayController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
 using Cats.Models;
 using Cats.Services.EarlyWarning;
 using Cats.Models.ViewModels;
+using Newtonsoft.Json;
 
 namespace Cats.Controllers
 {
@@ -21,16 +26,17 @@ namespace Cats.Controllers
             _fdpService = fdpService;
         }
 
-        public JsonResult Index()
+        public string Index()
         {
             var messages = new List<SmsOutgoingMessage>();
-            var events = new List<SmsEventSend>();
-            
+            var sendevents = new List<SmsEventSend>();
+            var pack = new List<List<SmsEventSend>>();
+
             var messageOne = new SmsOutgoingMessage()
                 {
                     id = "4f7c9cea5e11b",
                     message = "Hello this is the first ever message from the original CATS",
-                    to = "0911663223",
+                    to = "251911663223",
                 };
 
             messages.Add(messageOne);
@@ -41,25 +47,78 @@ namespace Cats.Controllers
                     messages = messages
                 };
 
-            
-            events.Add(ev);
+            sendevents.Add(ev);
 
-            var s =(events= (
-                from evt in events
-                             select new SmsEventSend()
-                                 {
-                                     @event = ev.@event,
-                                     messages = ev.messages;
-                                 }
-                  ) 
-            );
+            dynamic collectionWrapper = new
+            {
 
-            //eventsw.Add(ev);
+                events = sendevents
 
-            return Json(s, JsonRequestBehavior.AllowGet);
+            };
+
+            var output = JsonConvert.SerializeObject(collectionWrapper);
+            var o = JsonConvert.SerializeObject(collectionWrapper, Newtonsoft.Json.Formatting.Indented);
+            //string result = o.ToString();
+            //return o;
+
+            string xml;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var sw = new StreamWriter(ms);
+
+                var settings = new XmlWriterSettings();
+                settings.Encoding = Encoding.UTF8;
+                settings.Indent = true;
+
+                //var s = new StringBuilder();
+                //var setting = new XmlWriterSettings { Encoding = Encoding.UTF8, Indent = true };
+                XmlWriter writer = XmlWriter.Create(sw, settings);
+
+                using (writer)
+                {
+
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("response");
+
+                    foreach (var eve in sendevents)
+                    {
+                        writer.WriteStartElement("messages");
+                        foreach (var sms in eve.messages)
+                        {
+                            writer.WriteStartElement("sms");
+
+                            //id="52d7924725bf0" to="251911663223">Hello Yareda CATS wishs you a great lunch!! ENVAYASMS is working now!
+
+                            writer.WriteAttributeString("id", sms.id);
+                            writer.WriteAttributeString("to", sms.to);
+                            writer.WriteRaw(sms.message);
+                            writer.WriteEndElement();
+                        }
+                        writer.WriteEndElement();
+                    }
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+
+                using (StreamReader sr = new StreamReader(ms))
+                {
+                    ms.Position = 0;
+                    xml = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+
+            return xml;
+
+            //string re = "<?xml version='1.0' encoding='UTF-8'?><response><messages><sms id="52d7924725bf0" to="251911663223
+            //">Hello Yareda CATS wishs you a great lunch!! ENVAYASMS is working now!</sms></messages></response>";
+
         }
 
-        public JsonResult Send()
+
+        public ActionResult Send()
         {
             var fdps = _fdpService.GetAllFDP().Take(2);
             var hh = (from fdp in fdps
@@ -70,7 +129,7 @@ namespace Cats.Controllers
                           }
                      );
             return Json(hh, JsonRequestBehavior.AllowGet);
+           // return View();
         }
-
     }
 }
