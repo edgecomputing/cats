4a2a35a63d85ef04c4028b4a09a10b391c959dd8
dawit demissie <dawitdb@yahoo.com>
2014-01-21 07:37:22 -0800

    add models/servces/tables/methods to bring requisitions by region

---
+++ Models/Cats.Models/DetailDistribution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models
+{
+    public partial class DetailDistribution
+    {
+        public int DistributionDetailId { get; set; }
+        public int DistributionHeaderId { get; set; }
+        public int FdpId { get; set; }
+        public decimal DistributedQuantity { get; set; }
+        public virtual FDP FDP { get; set; }
+        public virtual HeaderDistribution HeaderDistribution { get; set; }
+    }
+}
