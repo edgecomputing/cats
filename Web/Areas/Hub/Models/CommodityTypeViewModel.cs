0016b4f1790334e8c5efb894434c849b39077a21
nathnael <natash1145@gmail.com>
2014-01-29 09:48:36 +0300

    some changes on hub receive controller

---
+++ Web/Areas/Hub/Models/CommodityTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Areas.Hub.Models
+{
+    public class CommodityTypeViewModel
+    {
+        public int CommodityTypeID { get; set; }
+        public string Name { get; set; }
+    }
+}
