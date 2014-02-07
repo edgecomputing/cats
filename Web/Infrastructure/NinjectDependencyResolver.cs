02d4035d9eb72b47768f9be3644301bfa705d6a9
Fisseha Abebe <abebefisseha5@gmail.com>
2014-02-07 15:33:27 +0300

    Modified WebConfig File

--- Web/Infrastructure/NinjectDependencyResolver.cs
+++ Web/Infrastructure/NinjectDependencyResolver.cs
@@ -241,6 +241,7 @@ namespace Cats.Infrastructure
             kernel.Bind<ILocalPurchaseService>().To<LocalPurchaseService>();
             kernel.Bind<ILocalPurchaseDetailService>().To<LocalPurchaseDetailService>();
             kernel.Bind<IDonationPlanDetailService>().To<DonationPlanDetailService>();
+            kernel.Bind<IDonationPlanHeaderService>().To<DonationPlanHeaderService>();
         }
         private void AddBindingsHub()
         {
