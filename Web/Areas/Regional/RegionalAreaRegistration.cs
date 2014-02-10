3fbc9efbb045ca25492decbae36e36e0167ea24b
robi <blessedethio@gmail.com>
2014-02-10 15:44:44 +0300

    Refactor PSNP List page and add Regional Area plus massive ORM class

---
+++ Web/Areas/Regional/RegionalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Cats.Areas.Regional
+{
+    public class RegionalAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Regional";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Regional_default",
+                "Regional/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
