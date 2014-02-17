c540ad662c595b08fa0c66b0e08d07dff1e8b638
dawit demissie <dawitdb@yahoo.com>
2014-02-17 04:55:19 -0800

    add another error logging code for debugging purpose

--- Infrastructure/Cats.Documents/DocumentGenerator.cs
+++ Infrastructure/Cats.Documents/DocumentGenerator.cs
@@ -98,6 +98,7 @@ namespace Cats.Documents
                 // Don't continue if the template file name is not found
                 if (!File.Exists(_templateFileName))
                 {
+                    System.IO.File.AppendAllText(@"c:\temp\errors.txt","In GenerateDocument:   " +  _templateFileName);
                     throw new Exception(message: "TemplateFileName (" + _templateFileName + ") does not exist");
                   
                 }
