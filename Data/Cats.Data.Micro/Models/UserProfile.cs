451145a7072345a752963449ab084ce7f3768833
dawit demissie <dawitdb@yahoo.com>
2014-02-17 00:55:32 -0800

    resolve conflict

---
+++ Data/Cats.Data.Micro/Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿using Cats.Cats.Data.Micro;
+
+namespace Cats.Data.Micro.Models
+{
+    public class UserProfile : DynamicModel
+    {
+        //you don't have to specify the connection - Massive will use the first one it finds in your config
+        public UserProfile() : base("CatsContext", "UserProfile", "UserProfileID") { }
+    }
+}
--- Services/Cats.Services.Dashboard/RegionalDashboard.cs
+++ Services/Cats.Services.Dashboard/RegionalDashboard.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Cats.Data.Micro;
+using Cats.Data.Micro.Models;
 
 namespace Cats.Services.Dashboard
 {
