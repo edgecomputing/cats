0958d417abb5a79ff6a3456cf81fbb31260af022
Yared Ayalew <yareda@gmail.com>
2014-02-18 14:22:33 +0300

    Refactor Request search page to align controls on a single row

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -60,8 +60,8 @@ namespace Cats
         {
             if (Request.IsLocal)
             {
-                //MiniProfiler.Start();
-                //MiniProfilerEF.InitializeEF42();
+                MiniProfiler.Start();
+                MiniProfilerEF.InitializeEF42();
             }
         }
 
