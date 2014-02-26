af6d416b8fdae8ea8ad9f1bb5d99c149d5db00f8
dawit demissie <dawitdb@yahoo.com>
2014-02-26 07:05:48 -0800

    add angular controllers / server side action methods to fill ldashboard view

--- Web/Areas/Logistics/Controllers/HomeController.cs
+++ Web/Areas/Logistics/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Cats.Services.EarlyWarning;
+using Cats.Services.Logistics;
 using Cats.Services.Procurement;
 using Cats.Services.Security;
 using hub = Cats.Services.Hub;
@@ -22,14 +23,16 @@ namespace Cats.Areas.Logistics.Controllers
         private readonly ITransportOrderDetailService _transportOrderDetailService;
         private readonly hub.IDispatchService _dispatchService;
         private readonly hub.IDispatchDetailService _dispatchDetailService;
-       
+        private readonly Cats.Services.Logistics.ISIPCAllocationService _sipcAllocationService;
+        private readonly IAdminUnitService _adminUnitService;
+
         public HomeController(IReliefRequisitionService reliefRequisitionService,
             hub.IDispatchAllocationService dispatchAllocationService,
             IUserAccountService userAccountService,
             ITransportOrderService transportOrderService,
             ITransportOrderDetailService transportOrderDetailService,
             hub.DispatchService dispatchService,
-            hub.DispatchDetailService dispatchDetailService)
+            hub.DispatchDetailService dispatchDetailService, ISIPCAllocationService sipcAllocationService, IAdminUnitService adminUnitService)
         {
             this._reliefRequisitionService = reliefRequisitionService;
             _dispatchAllocationService = dispatchAllocationService;
@@ -37,7 +40,9 @@ namespace Cats.Areas.Logistics.Controllers
             _transportOrderService = transportOrderService;
             _transportOrderDetailService = transportOrderDetailService;
             _dispatchService = dispatchService;
-            _dispatchDetailService = dispatchDetailService; 
+            _dispatchDetailService = dispatchDetailService;
+            _sipcAllocationService = sipcAllocationService;
+            _adminUnitService = adminUnitService;
         }
 
         public ActionResult Index()
@@ -150,18 +155,62 @@ namespace Cats.Areas.Logistics.Controllers
 
         #region "Dashboard"
 
+        public JsonResult GetRegions()
+        {
+            var regions = _adminUnitService.GetRegions().Select(r=> new
+                                                                        {
+                                                                            name = r.Name,
+                                                                            id=r.AdminUnitID
+                                                                        });
+            return Json(regions, JsonRequestBehavior.AllowGet);
+        }
+
         public JsonResult ImportantNumbers ()
         {
             var requests =
                 _reliefRequisitionService.GetAllReliefRequisition().GroupBy(s => s.Status).Select(c=> new
                                                                                                           {
                                                                                                               Status = c.Key,
-                                                                                                              Count =c.Count()
-
-
+                                                                                                              Count =c.Count(),
+                                                                                                              regionId = c.Select(d=>d.RegionID),
+                                                                                                              RegionName = c.Select(d=>d.AdminUnit.Name)
                                                                                                           });
             return Json(requests, JsonRequestBehavior.AllowGet);
         }
+
+
+        public JsonResult GetRequiasitions(int id)
+        {
+            var requestes = _reliefRequisitionService.FindBy(s => s.Status == id).Select(r => new
+                                                                                                  {
+                                                                                                      reqNo= r.RequisitionNo,
+                                                                                                      zone= r.AdminUnit1.Name,
+                                                                                                      beneficiaries = r.ReliefRequisitionDetails.Sum(d=>d.BenficiaryNo),
+                                                                                                      amount  = r.ReliefRequisitionDetails.Sum(d=>d.Amount),
+                                                                                                      commodity = r.Commodity.Name,
+                                                                                                      regionId  = r.RegionalRequest.RegionID,
+                                                                                                      RegionName = r.RegionalRequest.AdminUnit.Name
+                                                                                                  });
+            return Json(requestes, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetSiPcAllocation()
+        {
+            var siPcAllocated = _sipcAllocationService.GetAll().Select(s => new
+                                                                                {
+                                                                                    reqNo = s.ReliefRequisitionDetail.ReliefRequisition.RequisitionNo,
+                                                                                    beneficiaries = s.ReliefRequisitionDetail.BenficiaryNo,
+                                                                                    amount = s.AllocatedAmount,
+                                                                                    commodity = s.ReliefRequisitionDetail.ReliefRequisition.Commodity.Name,
+                                                                                    allocationType = s.AllocationType,
+                                                                                    regionId = s.ReliefRequisitionDetail.ReliefRequisition.RegionID,
+                                                                                    RegionName =s.ReliefRequisitionDetail.ReliefRequisition.AdminUnit.Name
+                                                                                });
+            return Json(siPcAllocated, JsonRequestBehavior.AllowGet);
+        }
+
+
+        
         #endregion
     }
 }
