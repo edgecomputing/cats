6988faa0d7c8a75333f4822650ccde6f3d6df05d
robi <blessedethio@gmail.com>
2014-02-26 10:21:52 +0300

    Regional dashboard

---
+++ Data/Cats.Data.Micro/Models/RecentDispatches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cats.Cats.Data.Micro;
+
+namespace Cats.Data.Micro.Models
+{
+    public class RecentDispatches : DynamicModel
+    {
+        public RecentDispatches()
+            : base("CatsContext", "Dashboard_Regional_Dispatches")
+        {
+
+        }
+    }
+}
---
+++ Data/Cats.Data.Micro/Models/StatusPercentage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Data.Micro.Models
+{
+    public class StatusPercentage
+    {
+        public string Status { get; set; }
+        public int Count { get; set; }
+        public int RegionID { get; set; }
+    }
+}
