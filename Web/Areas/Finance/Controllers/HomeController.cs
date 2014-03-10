9043a5d63c8d702c6e43f48b1e861494971a6972
dawit demissie <dawitdb@yahoo.com>
2014-03-10 06:14:18 -0700

    add new finaance area and new index view for finance dashboard

---
+++ Web/Areas/Finance/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cats.Areas.Finance.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Finance/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
