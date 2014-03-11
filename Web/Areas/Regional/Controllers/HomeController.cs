bb3691484385a2aefd657bfbddbae4da1473e817
robi <blessedethio@gmail.com>
2014-03-11 10:25:37 +0300

    after demo

--- Web/Areas/Regional/Controllers/HomeController.cs
+++ Web/Areas/Regional/Controllers/HomeController.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cats.Helpers;
+using Cats.Models.Security;
 using Cats.Services.Dashboard;
+using Cats.Services.EarlyWarning;
 
 namespace Cats.Areas.Regional.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAdminUnitService _adminUnitService;
+
+        public HomeController(IAdminUnitService adminUnitService)
+        {
+            _adminUnitService = adminUnitService;
+        }
         //
+
         // GET: /Regional/Home/
         public ActionResult Index()
         {
-            ViewBag.RegionID = 2;
-            ViewBag.RegionName = "Afar";
+            var currentUser = UserAccountHelper.GetUser(HttpContext.User.Identity.Name);
+            ViewBag.RegionName = _adminUnitService.FindById(currentUser.RegionID??0).Name;
             return View();
         }
-
        
     }
 }
