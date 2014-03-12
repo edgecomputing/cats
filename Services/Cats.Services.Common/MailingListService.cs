ce67c30ae5c556dcb99055356df39e1e958bc7e5
Beka Mengistu <bekalive@gmail.com>
2014-03-12 12:38:46 +0300

    old commits

---
+++ Services/Cats.Services.Common/MailingListService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cats.Models;
+using Cats.Data.UnitWork;
+
+namespace Cats.Services.Common
+{
+    public class MailingListService:IMailingListService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MailingListService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public ICollection<Contact> GetContactsForMailingList(int MailingListID)
+        {
+            List<UserProfile> users= _unitOfWork.UserProfileRepository.FindBy(u => u.MobileNumber != null);
+
+            return (from item in users
+                    select new Contact()
+                    {
+                        ContactID= item.UserProfileID
+                        ,FirstName=item.FirstName
+                        ,LastName=item.LastName
+                        ,Email=item.Email
+                        ,MobileNumber=item.MobileNumber
+                    }
+                    ).ToList();
+        }
+    }
+}
