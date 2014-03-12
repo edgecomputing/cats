39ee406179c270bc55f515e55bc6cccba0a94ce3
natash1145 <natash1145@gmail.com>
2014-03-12 18:07:29 +0300

    fixed some conflicts

--- Services/Cats.Services.Hub/DispatchAllocationService.cs
+++ Services/Cats.Services.Hub/DispatchAllocationService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Cats.Data.Hub;
+using Cats.Data.Hub.UnitWork;
 using Cats.Models.Hubs;
 using Cats.Models.Hubs.ViewModels;
 using Cats.Models.Hubs.ViewModels.Common;
