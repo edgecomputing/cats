5d6e2b3d85cd2ed3d71ba46bf40212cebdb82bf1
natash1145 <natash1145@gmail.com>
2014-03-12 14:55:37 +0300

    CATS-956 Manage Payment Requests,

---
+++ Web/Areas/Procurement/Controllers/ValidatedPaymentRequestController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cats.Areas.Procurement.Models;
+using Cats.Helpers;
+using Cats.Models;
+using Cats.Models.Constant;
+using Cats.Services.Common;
+using Cats.Services.EarlyWarning;
+using Cats.Services.Procurement;
+using Cats.ViewModelBinder;
+
+namespace Cats.Areas.Procurement.Controllers
+{
+    public class ValidatedPaymentRequestController : Controller
+    {
+        private readonly IBusinessProcessService _businessProcessService;
+        private readonly IBusinessProcessStateService _businessProcessStateService;
+        private readonly IApplicationSettingService _applicationSettingService;
+        private readonly IPaymentRequestService _paymentRequestservice;
+        private readonly ITransportOrderService _transportOrderService;
+        private readonly ITransporterAgreementVersionService _transporterAgreementVersionService;
+        private readonly IWorkflowStatusService _workflowStatusService;
+        private readonly ITransporterService _transporterService;
+
+        public ValidatedPaymentRequestController(IBusinessProcessService paramBusinessProcessService
+                                        , IBusinessProcessStateService paramBusinessProcessStateService
+                                        , IApplicationSettingService paramApplicationSettingService
+                                        , IPaymentRequestService paramPaymentRequestservice
+                                        , ITransportOrderService paramTransportOrderService
+                                        , ITransporterAgreementVersionService transporterAgreementVersionService
+                                        , IWorkflowStatusService workflowStatusService, ITransporterService transporterService)
+            {
+
+                _businessProcessService = paramBusinessProcessService;
+                _businessProcessStateService = paramBusinessProcessStateService;
+                _applicationSettingService = paramApplicationSettingService;
+                _paymentRequestservice = paramPaymentRequestservice;
+                _transportOrderService = paramTransportOrderService;
+                _transporterAgreementVersionService = transporterAgreementVersionService;
+                _workflowStatusService = workflowStatusService;
+                _transporterService = transporterService;
+            }
+        //
+        // GET: /Procurement/ValidatedPaymentRequest/
+
+        public ActionResult Index()
+        {
+            ViewBag.TargetController = "ValidatedPaymentRequest";
+            var list = (IEnumerable<Cats.Models.PaymentRequest>)_paymentRequestservice.Get(t => t.BusinessProcess.CurrentState.BaseStateTemplate.StateNo >= 2, null, "BusinessProcess").OrderByDescending(t => t.PaymentRequestID);
+            return View(list);
+        }
+
+        public ActionResult Promote(BusinessProcessState st)
+        {
+            _businessProcessService.PromotWorkflow(st);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult TransportOrderDetail(int transportOrderID)
+        {
+            var transportOrder = _transportOrderService.Get(t => t.TransportOrderID == transportOrderID, null, "TransportOrderDetails.FDP,TransportOrderDetails.FDP.AdminUnit,TransportOrderDetails.Commodity,TransportOrderDetails.Hub,TransportOrderDetails.ReliefRequisition").FirstOrDefault();
+            var datePref = UserAccountHelper.GetUser(User.Identity.Name).DatePreference;
+            var statuses = _workflowStatusService.GetStatus(WORKFLOW.TRANSPORT_ORDER);
+            var transportOrderViewModel = TransportOrderViewModelBinder.BindTransportOrderViewModel(transportOrder, datePref, statuses);
+            ViewData["Transport.order.detail.ViewModel"] = transportOrder == null ? null :
+                GetDetail(transportOrder.TransportOrderDetails);
+            return View(transportOrderViewModel);
+        }
+
+        public ActionResult TransporterDetail(int transporterID)
+        {
+            var transporter = _transporterService.FindById(transporterID);
+            if (transporter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transporter);
+        }
+
+        private IEnumerable<TransportOrderDetailViewModel> GetDetail(IEnumerable<TransportOrderDetail> transportOrderDetails)
+        {
+
+            var transportOrderDetailViewModels =
+                (from itm in transportOrderDetails select BindTransportOrderDetailViewModel(itm));
+            return transportOrderDetailViewModels;
+        }
+        private TransportOrderDetailViewModel BindTransportOrderDetailViewModel(TransportOrderDetail transportOrderDetail)
+        {
+            TransportOrderDetailViewModel transportOrderDetailViewModel = null;
+            if (transportOrderDetail != null)
+            {
+                transportOrderDetailViewModel = new TransportOrderDetailViewModel
+                                                    {
+                                                        FdpID = transportOrderDetail.FdpID,
+                                                        FDP = transportOrderDetail.FDP.Name,
+                                                        CommodityID = transportOrderDetail.CommodityID,
+                                                        Commodity = transportOrderDetail.Commodity.Name,
+                                                        DonorID = transportOrderDetail.DonorID,
+                                                        OriginWarehouse = transportOrderDetail.Hub.Name,
+                                                        QuantityQtl = transportOrderDetail.QuantityQtl,
+                                                        RequisitionID = transportOrderDetail.RequisitionID,
+                                                        RequisitionNo =
+                                                            transportOrderDetail.ReliefRequisition.RequisitionNo,
+                                                        SourceWarehouseID = transportOrderDetail.SourceWarehouseID,
+                                                        TariffPerQtl = transportOrderDetail.TariffPerQtl,
+                                                        Woreda = transportOrderDetail.FDP.AdminUnit.Name
+                                                    };
+            }
+            return transportOrderDetailViewModel;
+        }
+
+        public void ViewContractAgreement(int transporterID)
+        {
+            // TODO: Make sure to use DI to get the template generator instance
+
+            var transportAgreementVersionObj = _transporterAgreementVersionService.Get(t=>t.TransporterID == transporterID && t.Current).FirstOrDefault();
+            //var filePath = template.GenerateTemplate(transporterID, 7, "FrameworkPucrhaseContract"); //here you have to send the name of the tempalte and the id of the TransporterID
+
+            if (transportAgreementVersionObj != null)
+            {
+                var data = (byte[])transportAgreementVersionObj.AgreementDocxFile;
+                var guid = new Guid();
+                var documentPath =
+                    System.Web.HttpContext.Current.Server.MapPath(string.Format("~/Templates/{0}.docx", Guid.NewGuid().ToString()));
+                using (var stream = new FileStream(documentPath, FileMode.Create))
+                {
+                    stream.Write(data, 0, data.Length);
+                };
+
+                Response.Clear();
+                Response.ContentType = "application/text";
+                Response.AddHeader("Content-Disposition", @"filename= FrameworkPurchaseContract.docx");
+                Response.TransmitFile(documentPath);
+            }
+            Response.End();
+        }
+
+    }
+}
