73b91211ddd165ff937958f04af910d6d161e6d0
robi-github <blessedethio@gmail.com>
2014-03-28 10:38:44 +0300

    CIT-341 remove remember me from the login screen

--- Web/Models/ViewModels/LoginModel.cs
+++ Web/Models/ViewModels/LoginModel.cs
@@ -9,6 +9,6 @@ namespace Cats.Models.ViewModels
     {
         public string UserName { get; set; }
         public string Password { get; set; }
-        public bool RememberMe { get; set; }
+        //public bool RememberMe { get; set; }
     }
 }
