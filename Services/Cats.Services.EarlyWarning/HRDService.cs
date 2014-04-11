4a5b915f39cb6e23821742c3ae2e3fbd41691fd3
Fisseha Abebe <abebefisseha5@gmail.com>
2014-04-11 17:33:47 +0300

    Changed Starting month value based on plan startDate (month field)

--- Services/Cats.Services.EarlyWarning/HRDService.cs
+++ Services/Cats.Services.EarlyWarning/HRDService.cs
@@ -128,6 +128,7 @@ namespace Cats.Services.EarlyWarning
 
         public bool AddHRD(int year, int userID, int seasonID, int rationID, int planID)
         {
+            var plan = _unitOfWork.PlanRepository.FindById(planID);
             var woredas = _unitOfWork.AdminUnitRepository.FindBy(m => m.AdminUnitTypeID == 4);
             var hrd = new HRD()
                 {
@@ -144,7 +145,7 @@ namespace Cats.Services.EarlyWarning
                               select new HRDDetail
                               {
                                   WoredaID = detail.AdminUnitID,
-                                  StartingMonth = 1,
+                                  StartingMonth = plan.StartDate.Month,
                                   NumberOfBeneficiaries = 0,
                                   DurationOfAssistance = 0
                               }).ToList();
