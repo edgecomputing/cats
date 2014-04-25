9551cfd384a7e938b1576558490b3e50b108b71e
robi <blessedethio@gmail.com>
2014-04-25 16:32:07 +0300

    fixes on the demo day

---
+++ Models/Cats.Models/ViewModels/Dashboard/RecentRequests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models.ViewModels.Dashboard
+{
+    public class RecentRequests
+    {
+        public int RegionalRequestID { get; set; }
+        public string RequestNumber { get; set; }
+        public int Month { get; set; }
+        public DateTime RequestDate { get; set; }
+        public int Status { get; set; }
+        public int Beneficiaries { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class RecentRequisitions
+    {
+        public int RequisitionID { get; set; }
+        public string RequisitionNo { get; set; }
+        public int BenficiaryNo { get; set; }
+        public decimal Amount { get; set; }
+        public int? Status { get; set; }
+        public DateTime? RequestedDate { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
--- Services/Cats.Services.Dashboard/RegionalDashboard.cs
+++ Services/Cats.Services.Dashboard/RegionalDashboard.cs
@@ -5,32 +5,82 @@ using System.Text;
 using System.Threading.Tasks;
 using Cats.Data.Micro;
 using Cats.Data.Micro.Models;
+using Cats.Data.UnitWork;
+using Cats.Models;
+using Cats.Models.ViewModels.Dashboard;
 
 
 namespace Cats.Services.Dashboard
 {
     public class RegionalDashboard : IRegionalDashboard
     {
-        public List<Object> GetRecentRequests(int regionID)
+        private IUnitOfWork _unitOfWork;
+        
+        public RegionalDashboard(IUnitOfWork unitOfWork)
         {
-            //regionID = 5;
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<RecentRequests> GetRecentRequests(int regionID)
+        {
+            /*//regionID = 5;
             var requests = new RegionalRequest();
             //var result = requests.All( columns:"RequestNumber",where:"WHERE RegionID=@0",args:regionID);
-            var limResult = requests.Query("SELECT TOP 5 RegionalRequestID,RequestNumber,Month,RequestDate,Status FROM EarlyWarning.RegionalRequest WHERE RegionID=@0 ORDER BY RequestDate", args: regionID);
+            var limResult = requests.Query("SELECT TOP 5 RegionalRequestID,RequestNumber,Month,RequestDate,Status FROM EarlyWarning.RegionalRequest WHERE RegionID=@0 ORDER BY RegionalRequestID DESC", args: regionID);
             return limResult.ToList();
 
             //dynamic table = new RegionalRequest();
-            //var re = table.Find(Categor)
+            //var re = table.Find(Categor)*/
+            var r = new List<RecentRequests>();
+            var currentHRD = _unitOfWork.HRDRepository.FindBy(m => m.Status == 3).FirstOrDefault();
+            var requests =  _unitOfWork.RegionalRequestRepository.FindBy(t => t.RegionID == regionID && t.PlanID==currentHRD.PlanID).OrderByDescending(t=>t.RegionalRequestID).Take(5);
+           
+            foreach (var regionalRequest in requests)
+            {
+                var n = new RecentRequests
+                    {
+                        RegionalRequestID = regionalRequest.RegionalRequestID,
+                        RequestNumber = regionalRequest.ReferenceNumber,
+                        Month = regionalRequest.RegionalRequestDetails.Count(),
+                        Status = regionalRequest.Status,
+                        RequestDate = regionalRequest.RequistionDate,
+                        Beneficiaries = regionalRequest.RegionalRequestDetails.Sum(t => t.Beneficiaries),
+                        Amount = regionalRequest.RegionalRequestDetails.Sum(t=>t.RequestDetailCommodities.Sum(s=>s.Amount))
+                    };
+
+                r.Add(n);
+            }
+
+            return r;
         }
-        
-        public List<object> GetRecentRequisitions(int regionID)
+
+        public List<RecentRequisitions> GetRecentRequisitions(int regionID)
         {
-            var requisitions = new RecentRequisition();
+            /*var requisitions = new RecentRequisition();
             var limResult =
                 requisitions.Query(
-                    "SELECT TOP 5 * FROM Dashborad_Regional_Requisitions WHERE RegionID=@0 ORDER BY RequestedDate",
+                    "SELECT TOP 5 * FROM Dashborad_Regional_Requisitions WHERE RegionID=@0 ORDER BY RequestedDate DESC",
                     args: regionID);
-            return limResult.ToList();
+            return limResult.ToList();*/
+            var r = new List<RecentRequisitions>();
+            var currentHRD = _unitOfWork.HRDRepository.FindBy(m => m.Status == 3);
+            var requisitions = _unitOfWork.ReliefRequisitionRepository.FindBy(t => t.RegionID == regionID).OrderByDescending(t => t.RequisitionID).Take(5);
+
+            foreach (var regionalRequisition in requisitions)
+            {
+                var n = new Models.ViewModels.Dashboard.RecentRequisitions
+                    {
+                        RequisitionID = regionalRequisition.RequisitionID,
+                        RequisitionNo = regionalRequisition.RequisitionNo,
+                        Status = regionalRequisition.Status,
+                        RequestedDate = regionalRequisition.RequestedDate,
+                        BenficiaryNo = regionalRequisition.ReliefRequisitionDetails.Sum(t => t.BenficiaryNo),
+                        Name = regionalRequisition.Commodity.Name,
+                        Amount = regionalRequisition.ReliefRequisitionDetails.Sum(s=>s.Amount)
+                    };
+                r.Add(n);
+            }
+            return r;
         }
 
         public List<object> RequisitionsPercentage(int regionID)
@@ -52,10 +102,11 @@ namespace Cats.Services.Dashboard
             var requisitions = new RecentDispatches();
             var limResult =
                 requisitions.Query(
-                    "SELECT TOP 5 * FROM Dashboard_Regional_Dispatches WHERE RegionID=@0 ORDER BY DispatchDate",
+                    "SELECT TOP 5 * FROM Dashboard_Regional_Dispatches WHERE RegionID=@0 ORDER BY DispatchDate DESC",
                     args: regionID);
             return limResult.ToList();
         }
+
         public void Dispose()
         {
          
