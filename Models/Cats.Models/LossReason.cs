510326aede82557a2a9958f2d13d56794975556a
Fisseha abebe <abebefisseha5@gmail.com>
2014-06-03 11:20:00 +0300

    CIT-416 Added Loss Reason Model

---
+++ Models/Cats.Models/LossReason.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models
+{
+   public class LossReason
+    {
+       public int LossReasonId { get; set; }
+       public string LossReasonEg { get; set; }
+       public string LossReasonAm { get; set; }
+       public string LossReasonCodeEg { get; set; }
+       public string LossReasonCodeAm { get; set; }
+
+    }
+}
