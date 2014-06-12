1ae3daa587acf591cb071e75252dbdfde2916475
Beka Mengistu <bekalive@gmail.com>
2014-06-12 17:20:24 +0300

    RecieveController-Updated the index action method.

--- Web/Areas/Hub/Controllers/ReceiveController.cs
+++ Web/Areas/Hub/Controllers/ReceiveController.cs
@@ -292,14 +292,17 @@ namespace Cats.Areas.Hub.Controllers
         /// Shows a list of receive transactions.
         /// </summary>
         /// <returns></returns>
-
+        /*
         public virtual ActionResult Index()
         {
             UserProfile user = _userProfileService.GetUser(User.Identity.Name);
             List<Receive> receives = _receiveService.ByHubId(user.DefaultHub.HubID);
             return View(receives);
         }
-        public virtual ActionResult Index_NEW()
+         * */
+//        public virtual ActionResult Index_NEW()
+
+        public virtual ActionResult Index()
         {
             UserProfile user = _userProfileService.GetUser(User.Identity.Name);
             populateLookups(user);
