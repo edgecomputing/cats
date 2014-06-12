cef0248f892b9ce14cdd8a5b59ab591b8905e4b4
Fisseha abebe <abebefisseha5@gmail.com>
2014-06-12 16:15:26 +0300

    CIT-444 Resolved Psnp annual plan print out problem & report layout

--- Models/Cats.Models/vwPSNPAnnualPlan.cs
+++ Models/Cats.Models/vwPSNPAnnualPlan.cs
@@ -11,16 +11,17 @@ namespace Cats.Models
         public Nullable<int> FoodRatio { get; set; }
         public Nullable<int> CashRatio { get; set; }
         public Nullable<int> BeneficiaryCount { get; set; }
-        public Nullable<int> PlanedFDPID { get; set; }
+        //public Nullable<int> PlanedFDPID { get; set; }
         public Nullable<int> Duration { get; set; }
         //public Nullable<int> RegionID { get; set; }
         public Nullable<int> Year { get; set; }
         public int RegionalPSNPPlanID { get; set; }
         //public string RegionName { get; set; }
-        public string FDPName { get; set; }
+        //public string FDPName { get; set; }
         public string WoredaName { get; set; }
         public int WoredaID { get; set; }
         public int ZoneID { get; set; }
         public string ZoneName { get; set; }
+        public string RegionName { get; set; }
     }
 }
