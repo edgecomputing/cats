08b0391a8654d170986b601fa38328abc6dc6a9d
Beka Mengistu <bekalive@gmail.com>
2014-06-24 12:02:50 +0300

    Added Zone and Region ID in view model binding

--- Web/Areas/EarlyWarning/Controllers/RequestController.cs
+++ Web/Areas/EarlyWarning/Controllers/RequestController.cs
@@ -645,8 +645,8 @@ namespace Cats.Areas.EarlyWarning.Controllers
         {
 
             var requestDetails = _regionalRequestDetailService.FindBy(t => t.RegionalRequestID == id);
-            RegionalRequestDetailViewModel requestDetailViewModels = (from dtl in requestDetails select BindRegionalRequestDetailViewModel(dtl));
-            requestDetailViewModels.RegionalRequestID = id;
+            var requestDetailViewModels = (from dtl in requestDetails select BindRegionalRequestDetailViewModel(dtl));
+            //requestDetailViewModels.RegionalRequestID = id;
             return Json(requestDetailViewModels.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
         }
         
@@ -664,6 +664,8 @@ namespace Cats.Areas.EarlyWarning.Controllers
                     Woreda = regionalRequestDetail.Fdp.AdminUnit.Name,
                     WoredaId = regionalRequestDetail.Fdp.AdminUnit.AdminUnitID,
                     Zone = regionalRequestDetail.Fdp.AdminUnit.AdminUnit2.Name,
+                    ZoneId = regionalRequestDetail.Fdp.AdminUnit.AdminUnit2.AdminUnitID,
+                    RegionId = regionalRequestDetail.Fdp.AdminUnit.AdminUnit2.AdminUnit2.AdminUnitID,
                     //PlannedBeneficiaries = GetPlanned(regionalRequestDetail.RegionalRequest.Year,
                         //(int)regionalRequestDetail.RegionalRequest.Season,
                         //regionalRequestDetail.Fdp.AdminUnit.AdminUnitID)
