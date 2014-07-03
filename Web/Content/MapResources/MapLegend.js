d9943b4926a638fd950827b795623d28d1c4b35e
Beka Mengistu <bekalive@gmail.com>
2014-07-03 17:07:56 +0300

    Region Border style

--- Web/Content/MapResources/MapLegend.js
+++ Web/Content/MapResources/MapLegend.js
@@ -1,20 +1,23 @@
-function ShowLegend(colorOptions, div,data) {
+function ShowLegend(colorOptions, div, data) {
+    console.log("showLegend", data);
     colorOptions = extendColorOption(colorOptions);
+    var lastVal = "";
     var htm = "<div class='title'>Legend</div>";
     for (var i = 0; i < colorOptions.sample; i++) {
         var color = getRGBValue(colorOptions.h, colorOptions.s, colorOptions.b1, colorOptions.b2, colorOptions.sample-1, i / colorOptions.sample);
         var style = "background:" + color + ";";
         var txt = "From to ";
-        var val = data.maxValue;// data.minValue + (data.maxValue - data.minValue) * i / (colorOptions.sample - i);
+        var val = data.minVal + (data.maxVal - data.minVal) * i / (colorOptions.sample - i);
         val = Math.round(val);
-        var valFrom = 0;
-        var valTo = i;
-        if (i > 0) {
-            valFrom = i - 1;
-        }
         
-        htm += "<div class='item'><i class='pallet' style='" + style + "'>&nbsp;</i> From " + val + "</div>";
+        txt = "Less Than " + val;
+        if (lastVal) {
+            txt="From " + lastVal + " to " + val;
+        }
+        lastVal = val;
+        htm += "<div class='item'><i class='pallet' style='" + style + "'>&nbsp;</i> " + txt + "</div>";
     }
+    htm += "<div class='item'><i class='pallet' style='background:" + colorOptions.noValColor  + ";'>&nbsp;</i> No Data </div>";
     $("#" + div).html(htm);
     
     
