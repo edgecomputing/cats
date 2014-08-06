44f85ee8724dcdb41345e916ab982212cfd268eb
bcopmt143 <binwel2000@hotmail.com>
2014-08-06 06:44:23 -0500

    Resolve: CATS-1094

--- Web/Areas/Logistics/Controllers/HomeController.cs
+++ Web/Areas/Logistics/Controllers/HomeController.cs
@@ -29,12 +29,13 @@ namespace Cats.Areas.Logistics.Controllers
         private readonly ITransportOrderService _transportOrderService;
         private readonly ITransportOrderDetailService _transportOrderDetailService;
         private readonly hub.IDispatchService _dispatchService;
-        private readonly hub.IDispatchDetailService _dispatchDetailService;
-        private readonly Cats.Services.Logistics.ISIPCAllocationService _sipcAllocationService;
+        private readonly hub.DispatchDetailService _dispatchDetailService;
+        private readonly ISIPCAllocationService _sipcAllocationService;
         private readonly IAdminUnitService _adminUnitService;
         private readonly IHRDService _hrdService;
         private readonly IHRDDetailService _hrdDetailService;
         private readonly IRationDetailService _rationDetailService;
+        private readonly IProgramService _programService;
         private readonly IBidWinnerService _bidWinnerService;
         private readonly IBidService _bidService;
 
@@ -44,7 +45,15 @@ namespace Cats.Areas.Logistics.Controllers
             ITransportOrderService transportOrderService,
             ITransportOrderDetailService transportOrderDetailService,
             hub.DispatchService dispatchService,
-            hub.DispatchDetailService dispatchDetailService, ISIPCAllocationService sipcAllocationService, IAdminUnitService adminUnitService, IHRDService hrdService, IBidWinnerService bidWinnerService, IBidService bidService, IHRDDetailService hrdDetailService, IRationDetailService rationDetailService)
+            hub.DispatchDetailService dispatchDetailService,
+            ISIPCAllocationService sipcAllocationService,
+            IAdminUnitService adminUnitService,
+            IHRDService hrdService,
+            IBidWinnerService bidWinnerService,
+            IBidService bidService,
+            IHRDDetailService hrdDetailService,
+            IRationDetailService rationDetailService,
+            IProgramService programService)
         {
             this._reliefRequisitionService = reliefRequisitionService;
             _dispatchAllocationService = dispatchAllocationService;
@@ -60,6 +69,7 @@ namespace Cats.Areas.Logistics.Controllers
             _bidService = bidService;
             _hrdDetailService = hrdDetailService;
             _rationDetailService = rationDetailService;
+            _programService = programService;
         }
 
         public ActionResult Index()
@@ -75,7 +85,7 @@ namespace Cats.Areas.Logistics.Controllers
 
             var summary = GetHRDSummary(hrd.HRDID);
             return View(summary);
-           
+
         }
 
         public JsonResult GetRecievedRequisitions()
@@ -85,7 +95,7 @@ namespace Cats.Areas.Logistics.Controllers
         public JsonResult GetDispatchAllocationByHub(int hubId)
         {
             var user = _userAccountService.GetUserInfo(User.Identity.Name);
-           
+
             var fdpAllocations = _dispatchAllocationService.GetCommitedAllocationsByHubDetached(hubId, user.PreferedWeightMeasurment);
 
             return Json(fdpAllocations, JsonRequestBehavior.AllowGet);
@@ -101,34 +111,38 @@ namespace Cats.Areas.Logistics.Controllers
             var requisitions = from contract in contracts
                                select new
                                {
-                                       contract.TransportOrderNo,
-                                       details = from detail in
-                                                     _transportOrderDetailService.FindBy(
-                                                         d => d.TransportOrderID == contract.TransportOrderID)
-                                                 select
-                                                     new {detail.RequisitionID, detail.FDP.Name,
-                                                          sum = from dispatch in _dispatchService.FindBy(r => r.RequisitionNo == detail.ReliefRequisition.RequisitionNo)
-                                                            group dispatch by dispatch.RequisitionNo into d
-                                                            select new { 
-                                                                d.Key,
-                                                                d                      
-                                                            }
-                                                     }
+                                   contract.TransportOrderNo,
+                                   details = from detail in
+                                                 _transportOrderDetailService.FindBy(
+                                                     d => d.TransportOrderID == contract.TransportOrderID)
+                                             select
+                                                 new
+                                                 {
+                                                     detail.RequisitionID,
+                                                     detail.FDP.Name,
+                                                     sum = from dispatch in _dispatchService.FindBy(r => r.RequisitionNo == detail.ReliefRequisition.RequisitionNo)
+                                                           group dispatch by dispatch.RequisitionNo into d
+                                                           select new
+                                                           {
+                                                               d.Key,
+                                                               d
+                                                           }
+                                                 }
                                };
             //from requisition in requisitions 
             //select requisition.details.
             return Json(requisitions, JsonRequestBehavior.AllowGet);
         }
 
-        public  JsonResult GetTransportContractInfo()
+        public JsonResult GetTransportContractInfo()
         {
             var contracts = _transportOrderService.FindBy(t => t.StatusID >= 3);
             var info = (
-                        from contract in contracts 
+                        from contract in contracts
                         select new
                             {
                                 contract = contract.ContractNumber,
-                                transporter = contract.Transporter.Name ,
+                                transporter = contract.Transporter.Name,
                                 owner = contract.Transporter.OwnerName,
                                 //daysLeft = (int)(contract.EndDate - DateTime.Now).TotalDays,
                                 daysLeft = DaysLeft(contract),
@@ -141,16 +155,16 @@ namespace Cats.Areas.Logistics.Controllers
                                 percentage = ((contract.EndDate - DateTime.Now).TotalDays / (contract.EndDate - contract.StartDate).TotalDays) * 100
                             }
                        );
-            return Json(info,JsonRequestBehavior.AllowGet);
+            return Json(info, JsonRequestBehavior.AllowGet);
         }
 
-        public int DaysLeft( TransportOrder transportOrder)
+        public int DaysLeft(TransportOrder transportOrder)
         {
             var days = -1;
 
-            if ((int)(transportOrder.StartDate - DateTime.Now).TotalDays>0)
+            if ((int)(transportOrder.StartDate - DateTime.Now).TotalDays > 0)
             {
-                days = (int) (transportOrder.EndDate - DateTime.Now).TotalDays;
+                days = (int)(transportOrder.EndDate - DateTime.Now).TotalDays;
             }
             return days;
         }
@@ -183,12 +197,22 @@ namespace Cats.Areas.Logistics.Controllers
 
         public JsonResult GetRegions()
         {
-            var regions = _adminUnitService.GetRegions().Select(r=> new
+            var regions = _adminUnitService.GetRegions().Select(r => new
                                                                         {
                                                                             name = r.Name,
-                                                                            id=r.AdminUnitID
+                                                                            id = r.AdminUnitID
                                                                         });
             return Json(regions, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetRequestType()
+        {
+            var programTypes = _programService.GetAllProgram().Select(p => new
+            {
+                name = p.Name,
+                id = p.ProgramID
+            });
+            return Json(programTypes, JsonRequestBehavior.AllowGet);
         }
 
         #region hrd
@@ -217,7 +241,7 @@ namespace Cats.Areas.Logistics.Controllers
 
         public ActionResult CurrentHrdRead()
         {
-             var datePref = _userAccountService.GetUserInfo(HttpContext.User.Identity.Name).DatePreference;
+            var datePref = _userAccountService.GetUserInfo(HttpContext.User.Identity.Name).DatePreference;
             DateTime latestDate = _hrdService.Get(m => m.Status == 3).Max(m => m.PublishedDate);
             var hrds = _hrdService.FindBy(m => m.Status == 3 && m.PublishedDate == latestDate);
             var hrdsToDisplay = GetHrds(hrds).ToList();
@@ -242,10 +266,10 @@ namespace Cats.Areas.Logistics.Controllers
 
         public JsonResult GetBids()
         {
-            var bids = _bidService.GetAllBid().Where(b=>b.StatusID == (int) Cats.Models.Constant.BidStatus.Active).Select(b=> new
+            var bids = _bidService.GetAllBid().Where(b => b.StatusID == (int)Cats.Models.Constant.BidStatus.Active).Select(b => new
                                                                                                                          {
                                                                                                                              BidNo = b.BidNumber,
-                                                                                                                             BidId=b.BidID
+                                                                                                                             BidId = b.BidID
                                                                                                                          });
             return Json(bids, JsonRequestBehavior.AllowGet);
         }
@@ -256,7 +280,7 @@ namespace Cats.Areas.Logistics.Controllers
             return (from bidWinner in bidWinners
                     select new Cats.Models.ViewModels.TransporterViewModel()
                     {
-                        
+
                         TransporterID = bidWinner.TransporterID,
                         Name = bidWinner.Transporter.Name,
                         RegioName = _adminUnitService.FindById(bidWinner.Transporter.Region).Name,
@@ -270,16 +294,16 @@ namespace Cats.Areas.Logistics.Controllers
                         Fdp = bidWinner.AdminUnit.Name
                     }).Distinct();
         }
-        public JsonResult  GetListOfBidWinners(string id)
+        public JsonResult GetListOfBidWinners(string id)
         {
             var selectedBidWinners = new List<TransporterViewModel>();
             var bid = _bidService.FindBy(t => t.BidNumber == id).SingleOrDefault();
-            var bidWinner = _bidWinnerService.FindBy(m => m.BidID == bid.BidID );
+            var bidWinner = _bidWinnerService.FindBy(m => m.BidID == bid.BidID);
             if (bidWinner != null)
             {
                 selectedBidWinners = GetBidWinners(bidWinner).ToList();
             }
-            return Json(selectedBidWinners,JsonRequestBehavior.AllowGet);
+            return Json(selectedBidWinners, JsonRequestBehavior.AllowGet);
         }
 
 
@@ -288,29 +312,47 @@ namespace Cats.Areas.Logistics.Controllers
         #region request
 
 
-        public JsonResult ImportantNumbers (string id)
+        public JsonResult ImportantNumbers(string id, string requestName)
         {
-            var requests =
-                _reliefRequisitionService.GetAllReliefRequisition().Where(d => d.AdminUnit.Name == id).GroupBy(s => s.Status).Select(c => new
-                                                                                                           {
-                                                                                                               Status = c.Key,
-                                                                                                               Count = c.Count(),
-
-                                                                                                           });
-            return Json(requests, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(requestName))
+            {
+                var requests =
+                _reliefRequisitionService.GetAllReliefRequisition()
+                .Where(d => d.AdminUnit.Name == id)
+                .GroupBy(s => s.Status)
+                .Select(c => new
+                {
+                    Status = c.Key,
+                    Count = c.Count(),
+                });
+                return Json(requests, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var request =
+              _reliefRequisitionService.GetAllReliefRequisition()
+              .Where(d => d.AdminUnit.Name == id && d.Program.Name == requestName)
+              .GroupBy(s => s.Status)
+              .Select(c => new
+              {
+                  Status = c.Key,
+                  Count = c.Count(),
+              });
+                return Json(request, JsonRequestBehavior.AllowGet);
+            }
         }
 
 
         public JsonResult GetRequiasitions(int id)
         {
-            var requestes = _reliefRequisitionService.GetAllReliefRequisition().Where(s=>s.Status == id).Select(r => new
+            var requestes = _reliefRequisitionService.GetAllReliefRequisition().Where(s => s.Status == id).Select(r => new
                                                                                                   {
-                                                                                                      reqNo= r.RequisitionNo,
-                                                                                                      zone= r.AdminUnit1.Name,
-                                                                                                      beneficiaries = r.ReliefRequisitionDetails.Sum(d=>d.BenficiaryNo),
-                                                                                                      amount  = r.ReliefRequisitionDetails.Sum(d=>d.Amount),
+                                                                                                      reqNo = r.RequisitionNo,
+                                                                                                      zone = r.AdminUnit1.Name,
+                                                                                                      beneficiaries = r.ReliefRequisitionDetails.Sum(d => d.BenficiaryNo),
+                                                                                                      amount = r.ReliefRequisitionDetails.Sum(d => d.Amount),
                                                                                                       commodity = r.Commodity.Name,
-                                                                                                      regionId  = r.RegionalRequest.RegionID,
+                                                                                                      regionId = r.RegionalRequest.RegionID,
                                                                                                       RegionName = r.AdminUnit.Name
                                                                                                   });
             return Json(requestes, JsonRequestBehavior.AllowGet);
@@ -318,7 +360,7 @@ namespace Cats.Areas.Logistics.Controllers
 
         public JsonResult GetSiPcAllocation()
         {
-            var siPcAllocated = _sipcAllocationService.FindBy(r=>r.ReliefRequisitionDetail.ReliefRequisition.Status  == (int) Cats.Models.Constant.ReliefRequisitionStatus.ProjectCodeAssigned).Select(s => new
+            var siPcAllocated = _sipcAllocationService.FindBy(r => r.ReliefRequisitionDetail.ReliefRequisition.Status == (int)Cats.Models.Constant.ReliefRequisitionStatus.ProjectCodeAssigned).Select(s => new
                                                                                 {
                                                                                     reqNo = s.ReliefRequisitionDetail.ReliefRequisition.RequisitionNo,
                                                                                     beneficiaries = s.ReliefRequisitionDetail.BenficiaryNo,
@@ -326,7 +368,7 @@ namespace Cats.Areas.Logistics.Controllers
                                                                                     commodity = s.ReliefRequisitionDetail.ReliefRequisition.Commodity.Name,
                                                                                     allocationType = s.AllocationType,
                                                                                     regionId = s.ReliefRequisitionDetail.ReliefRequisition.RegionID,
-                                                                                    RegionName =s.ReliefRequisitionDetail.ReliefRequisition.AdminUnit.Name,
+                                                                                    RegionName = s.ReliefRequisitionDetail.ReliefRequisition.AdminUnit.Name,
                                                                                     program = s.ReliefRequisitionDetail.ReliefRequisition.Program.Name
                                                                                 });
             return Json(siPcAllocated, JsonRequestBehavior.AllowGet);
@@ -339,13 +381,14 @@ namespace Cats.Areas.Logistics.Controllers
         public JsonResult GetTransporters()
         {
             var transporters =
-                _transportOrderService.FindBy(s=>s.StatusID <  (int)(Cats.Models.Constant.TransportOrderStatus.Closed) ).Select(p => new{
-                                                                                  name = p.Transporter.Name,
-                                                                                  region = _adminUnitService.FindById(p.Transporter.Region).Name ,
-                                                                                  zone = _adminUnitService.FindById(p.Transporter.Zone).Name,
-                                                                                  transportOrderNo = p.TransportOrderNo,
-                                                                                  mobileNo = p.Transporter.MobileNo
-                                                                              });
+                _transportOrderService.FindBy(s => s.StatusID < (int)(Cats.Models.Constant.TransportOrderStatus.Closed)).Select(p => new
+                {
+                    name = p.Transporter.Name,
+                    region = _adminUnitService.FindById(p.Transporter.Region).Name,
+                    zone = _adminUnitService.FindById(p.Transporter.Zone).Name,
+                    transportOrderNo = p.TransportOrderNo,
+                    mobileNo = p.Transporter.MobileNo
+                });
             return Json(transporters, JsonRequestBehavior.AllowGet);
         }
         #endregion
