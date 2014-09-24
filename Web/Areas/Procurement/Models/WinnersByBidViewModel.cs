dd13201c71e17116d299d6115ba8b3ac733f9155
nathnael <natash1145@gmail.com>
2014-09-24 15:56:27 +0300

    Cancel first bid winner(s) and replace them with second winner(s)

--- Web/Areas/Procurement/Models/WinnersByBidViewModel.cs
+++ Web/Areas/Procurement/Models/WinnersByBidViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using Cats.Models;
 
 namespace Cats.Areas.Procurement.Models
@@ -26,5 +27,33 @@ namespace Cats.Areas.Procurement.Models
         public string TransporterName { get; set; }
         //public int BIDID { get; set; }
        
+    }
+
+    public class WoredaBidWinnerViewModel
+    {
+        public int BidID { get; set; }
+        public string BidNumber { get; set; }
+        public string SourceWarehouse { get; set; }
+        public int SourceId { get; set; }
+        public int DestinationId { get; set; }
+        public string Region { get; set; }
+        public string Zone { get; set; }
+        public string Woreda { get; set; }
+        public int LeavingTransporterID { get; set; }
+        public SelectList Transporters { get; set; }
+    }
+
+    public class WoredaCancelBidWinnerViewModel
+    {
+        public int BidID { get; set; }
+        public string BidNumber { get; set; }
+        public string SourceWarehouse { get; set; }
+        public int SourceId { get; set; }
+        public int DestinationId { get; set; }
+        public string Region { get; set; }
+        public string Zone { get; set; }
+        public string Woreda { get; set; }
+        public List<string> CanceledTransporters { get; set; }
+        public List<string> PromotedTransporters { get; set; }
     }
 }
