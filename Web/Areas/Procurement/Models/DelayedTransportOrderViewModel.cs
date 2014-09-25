e731b2b3b382bcd616f523fc2b528acb8e25f0dc
Biruk Tilahun <engbiruk@gmail.com>
2014-09-25 10:02:00 +0300

    Fix: CIT-609 logistics notification dashboard for delayed transporters

---
+++ Web/Areas/Procurement/Models/DelayedTransportOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Areas.Procurement.Models
+{
+    public class DelayedTransportOrderViewModel
+    {
+        public int TransportOrderID { get; set; }
+        public string TransportOrderNo { get; set; }
+        public string SignedDate { get; set; }
+        public string StartedOn { get; set; }
+        public string TransporterName { get; set; }
+        public string OrderExpiryDate { get; set; }
+        public double DaysPassedAfterSigned { get; set; }
+    }
+}
