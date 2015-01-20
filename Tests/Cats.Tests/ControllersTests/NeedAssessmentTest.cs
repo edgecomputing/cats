b558abe42df8b11e69775ea7058997dd015fcbbe
Fisseha Abebe <abebefisseha5@gmail.com>
2015-01-20 12:15:15 +0300

    Fix the failing needsAssessment Test

--- Tests/Cats.Tests/ControllersTests/NeedAssessmentTest.cs
+++ Tests/Cats.Tests/ControllersTests/NeedAssessmentTest.cs
@@ -11,6 +11,11 @@ using Kendo.Mvc.UI;
 using NUnit.Framework;
 using Moq;
 using log4net;
+using Cats.Services.Security;
+using Cats.Models.Security;
+using System.Web;
+using System.Security.Principal;
+using System.Web.Mvc;
 namespace Cats.Tests.ControllersTests
 {   [TestFixture]
     public class NeedAssessmentTest
@@ -237,6 +242,18 @@ NeedAssessmentHeader=new NeedAssessmentHeader(){AdminUnit=new AdminUnit(){Name="
                                           WorkflowID = 1
                                       }
                               };
+            var userAccountService = new Mock<IUserAccountService>();
+            userAccountService.Setup(t => t.GetUserInfo(It.IsAny<string>())).Returns(new UserInfo()
+            {
+                UserName = "x",
+                DatePreference = "en"
+            });
+            var fakeContext = new Mock<HttpContextBase>();
+            var identity = new GenericIdentity("User");
+            var principal = new GenericPrincipal(identity, null);
+            fakeContext.Setup(t => t.User).Returns(principal);
+            var controllerContext = new Mock<ControllerContext>();
+            controllerContext.Setup(t => t.HttpContext).Returns(fakeContext.Object);
             var commonService = new Mock<ICommonService>();
             commonService.Setup(t => t.GetAminUnits(It.IsAny<Expression<Func<AdminUnit, bool>>>(),
                       It.IsAny<Func<IQueryable<AdminUnit>, IOrderedQueryable<AdminUnit>>>(),
@@ -250,7 +267,7 @@ NeedAssessmentHeader=new NeedAssessmentHeader(){AdminUnit=new AdminUnit(){Name="
                                                                     needAssessmentDetailService.Object,
                                                                     seasonService.Object,
                                                                     typeOfNeedAssessmentService.Object,
-                                                                    log.Object, planService.Object, commonService.Object);
+                                                                    log.Object, planService.Object, commonService.Object,userAccountService.Object);
         }
         [TearDown]
         public void Dispose()
