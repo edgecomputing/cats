4f96b6c99cb4dabf5962bcba854bc6b3c3ea0492
Fisseha Abebe <abebefisseha5@gmail.com>
2015-02-26 12:01:07 +0300

    Added donor information in delivery table

--- Services/Cats.Services.Logistics/IDeliveryService.cs
+++ Services/Cats.Services.Logistics/IDeliveryService.cs
@@ -25,7 +25,7 @@ namespace Cats.Services.Logistics
             string includeProperties = "");
 
         decimal GetFDPDelivery(int transportOrderId, int fdpId);
-        int? GetDonorID(int id);
+        int? GetDonorID(string shippingInstruction);
 
     }
 }
