8decc6c4078472032d7ea7c5f1dc62bbcc0f43be
Fisseha Abebe <abebefisseha5@gmail.com>
2015-06-01 11:47:21 +0300

    Created Sample Amharic Report Viewer forGRNEntryReport

---
+++ Web/AmharicReportViewer.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AmharicReportViewer.aspx.cs" Inherits="Cats.AmharicReportViewer" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ScriptManager ID="ScriptManagerAmh" runat="server">
+        </asp:ScriptManager>
+        <rsweb:ReportViewer ID="ReportViewerAmh" runat="server" Height="149px" Width="805px">
+        </rsweb:ReportViewer>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Web/AmharicReportViewer.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Cats.Library;
+using Microsoft.Reporting.WebForms;
+
+namespace Cats
+{
+    public partial class AmharicReportViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            
+            ReportViewerAmh.ProcessingMode = ProcessingMode.Remote;
+   ReportViewerAmh.ServerReport.ReportServerUrl = new Uri("http://FISH:80/ReportServer");
+   
+   ReportViewerAmh.ServerReport.ReportPath = "/Logistics/GRNEntryReport";
+   ReportViewerAmh.ServerReport.Refresh();
+ 
+   ReportParameter[] reportParameterCollection = new ReportParameter[2];       //Array size describes the number of paramaters.
+   reportParameterCollection[0] = new ReportParameter();
+   reportParameterCollection[0].Name = "StartDate";                                 //Give Your Parameter Name
+   reportParameterCollection[0].Values.Add("12/12/2010");                         //Pass Parametrs's value here.
+
+   reportParameterCollection[1] = new ReportParameter();
+   reportParameterCollection[1].Name = "EndDate";                                 //Give Your Parameter Name
+   reportParameterCollection[1].Values.Add("12/12/2015");  
+   //ReportViewerAmh.ServerReport.SetParameters(reportParameterCollection);
+   ReportViewerAmh.ServerReport.Refresh();
+}
+
+        }
+    }
