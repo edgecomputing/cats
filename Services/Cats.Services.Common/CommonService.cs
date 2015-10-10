d95c96740ff5e1246026dd93c8f9b581bc28dc4a
dawit demissie <dawitdb@yahoo.com>
2015-10-10 14:49:08 +0300

    refactor regional request to bring only published HRDs when creating new regional requet

--- Services/Cats.Services.Common/CommonService.cs
+++ Services/Cats.Services.Common/CommonService.cs
@@ -96,7 +96,7 @@ namespace Cats.Services.Common
                var psnpPlanID = _unitOfWork.RegionalPSNPPlanRepository.FindBy(t => t.AttachedBusinessProcess.CurrentState.BaseStateTemplate.StateNo >2).Select(m => m.PlanId).Distinct();
                return _unitOfWork.PlanRepository.FindBy(m => psnpPlanID.Contains(m.PlanID) && m.ProgramID == programID && m.Status == (int)PlanStatus.PSNPCreated);
            }
-           var planId = _unitOfWork.HRDRepository.FindBy(m => m.Status ==(int)HRDStatus.Approved || m.Status ==(int)HRDStatus.Published).Select(m => m.PlanID).Distinct();
+           var planId = _unitOfWork.HRDRepository.FindBy(m=> m.Status ==(int)HRDStatus.Published).Select(m => m.PlanID).Distinct();
            return _unitOfWork.PlanRepository.FindBy(m => planId.Contains(m.PlanID) && m.ProgramID == programID && m.Status == (int)PlanStatus.HRDCreated);
        }
 
